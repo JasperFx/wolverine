a1666ad7629cb57773edfe72307359f969cd67a7
Jeremy D. Miller <jeremydmiller@yahoo.com>
2022-12-16 16:21:42 -0600

    No more auto-registration of the Rabbit MQ transport. Closes GH-111

---
+++ src/Transports/Wolverine.RabbitMQ.Tests/application_does_not_fail_without_rabbit_mq_transport_initialized.cs
@@ -0,0 +1,15 @@
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Xunit;
+
+namespace Wolverine.RabbitMQ.Tests;
+
+public class application_does_not_fail_without_rabbit_mq_transport_initialized
+{
+    [Fact]
+    public async Task do_not_fail()
+    {
+        using var host = await Host.CreateDefaultBuilder()
+            .UseWolverine().StartAsync();
+    }
+}
