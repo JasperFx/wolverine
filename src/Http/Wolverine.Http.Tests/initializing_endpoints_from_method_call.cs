682432819ca46aedf09c42f449fd3ef889afafa8
Jeremy D. Miller <jeremydmiller@yahoo.com>
2023-01-23 09:47:41 -0600

    First successful compilation of endpoint handlers!

--- src/Http/Wolverine.Http.Tests/initializing_endpoints_from_method_call.cs
+++ src/Http/Wolverine.Http.Tests/initializing_endpoints_from_method_call.cs
@@ -1,6 +1,6 @@
+using TestEndpoints;
 using JasperFx.CodeGeneration.Frames;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
 using Shouldly;
 using Microsoft.AspNetCore.Http;
@@ -32,7 +32,7 @@ public class initializing_endpoints_from_method_call
     public void determine_resource_type(string methodName, Type? expectedType)
     {
         var method = new MethodCall(typeof(FakeEndpoint), methodName);
-        var endpoint = new EndpointChain(method);
+        var endpoint = new EndpointChain(method, new EndpointGraph());
 
         if (expectedType == null)
         {
@@ -42,64 +42,6 @@ public class initializing_endpoints_from_method_call
         {
             endpoint.ResourceType.ShouldBe(expectedType);
         }
-    }
-}
-
-public class FakeEndpoint
-{
-    [HttpGet("/hello")]
-    public string SayHello()
-    {
-        return "Hello";
-    }
-    
-    [HttpGet("/hello/async")]
-    public Task<string> SayHelloAsync()
-    {
-        return Task.FromResult("Hello");
-    }
-    
-    [HttpGet("/hello/async2")]
-    public ValueTask<string> SayHelloAsync2()
-    {
-        return ValueTask.FromResult("Hello");
-    }
-
-    [HttpPost("/go")]
-    public void Go()
-    {
-        
-    }
-
-    [HttpPost("/go/async")]
-    public Task GoAsync()
-    {
-        return Task.CompletedTask;
-    }
-    
-    [HttpPost("/go/async2")]
-    public ValueTask GoAsync2()
-    {
-        return ValueTask.CompletedTask;
-    }
-
-    [HttpGet("/response")]
-    public BigResponse GetResponse()
-    {
-        return new BigResponse();
-    }
-    
-    [HttpGet("/response2")]
-    public Task<BigResponse> GetResponseAsync()
-    {
-        return Task.FromResult(new BigResponse());
-    }
-    
-        
-    [HttpGet("/response3")]
-    public ValueTask<BigResponse> GetResponseAsync2()
-    {
-        return ValueTask.FromResult(new BigResponse());
     }
 }
 
--- src/Http/Wolverine.Http/EndpointChain.cs
+++ src/Http/Wolverine.Http/EndpointChain.cs
@@ -2,7 +2,10 @@ using System.Linq.Expressions;
 using System.Reflection;
 using JasperFx.CodeGeneration;
 using JasperFx.CodeGeneration.Frames;
+using JasperFx.CodeGeneration.Model;
+using JasperFx.Core;
 using JasperFx.Core.Reflection;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.AspNetCore.Routing.Patterns;
 using Wolverine.Configuration;
@@ -11,12 +14,17 @@ namespace Wolverine.Http;
 
 public class EndpointChain : Chain<EndpointChain, ModifyEndpointAttribute>, ICodeFile
 {
-    public static EndpointChain ChainFor<T>(Expression<Action<T>> expression)
+    public static readonly Variable[] HttpContextVariables =
+        Variable.VariablesForProperties<HttpContext>(EndpointGraph.Context);
+    
+    private readonly EndpointGraph _parent;
+
+    public static EndpointChain ChainFor<T>(Expression<Action<T>> expression, EndpointGraph? parent = null)
     {
         var method = ReflectionHelper.GetMethod(expression);
         var call = new MethodCall(typeof(T), method);
 
-        return new EndpointChain(call);
+        return new EndpointChain(call, parent ?? new EndpointGraph());
     }
     
     private string _fileName;
@@ -24,11 +32,13 @@ public class EndpointChain : Chain<EndpointChain, ModifyEndpointAttribute>, ICod
     // Make the assumption that the route argument has to match the parameter name
     private readonly List<ParameterInfo> _routeArguments = new();
     private GeneratedType _generatedType;
+    private Type? _handlerType;
 
     public MethodCall Method { get; }
 
-    public EndpointChain(MethodCall method)
+    public EndpointChain(MethodCall method, EndpointGraph parent)
     {
+        _parent = parent;
         Method = method;
         
         // TODO -- need a helper for this in JasperFx.Core
@@ -51,8 +61,8 @@ public class EndpointChain : Chain<EndpointChain, ModifyEndpointAttribute>, ICod
 
         // RoutePatternFactory
     }
-    
-    public Type ResourceType { get; private set; }
+
+    public Type ResourceType { get; }
     
     public RoutePattern RoutePattern { get; }
 
@@ -70,19 +80,66 @@ public class EndpointChain : Chain<EndpointChain, ModifyEndpointAttribute>, ICod
     void ICodeFile.AssembleTypes(GeneratedAssembly assembly)
     {
         _generatedType = assembly.AddType(_fileName, typeof(EndpointHandler));
+        assembly.ReferenceAssembly(Method.HandlerType.Assembly);
+        assembly.ReferenceAssembly(typeof(HttpContext).Assembly);
+        assembly.ReferenceAssembly(typeof(EndpointChain).Assembly);
         
-        // START HERE. Figure out resource type, and what to do with that.
+        var handleMethod = _generatedType.MethodFor(nameof(EndpointHandler.Handle));
+        //handleMethod.AsyncMode = AsyncMode.AsyncTask; // Might not be necessary anymore
+        
+        handleMethod.DerivedVariables.AddRange(HttpContextVariables);
+
+        handleMethod.Frames.AddRange(DetermineFrames(assembly.Rules));
+    }
+
+    internal IEnumerable<Frame> DetermineFrames(GenerationRules rules)
+    {
+        // TODO -- have to take in IContainer later
+        
+        // TODO -- apply customizations from attributes if any
+        
+        // Add frames for any writers
+        if (ResourceType != typeof(void))
+        {
+            foreach (var writerPolicy in _parent.WriterPolicies)
+            {
+                if (writerPolicy.TryApply(this)) break;
+            }
+        }
+
+        foreach (var frame in Middleware)
+        {
+            yield return frame;
+        }
+
+        yield return Method;
+
+        foreach (var frame in Postprocessors)
+        {
+            yield return frame;
+        }
+
     }
 
     Task<bool> ICodeFile.AttachTypes(GenerationRules rules, Assembly assembly, IServiceProvider services, string containingNamespace)
     {
-        throw new NotImplementedException();
+        var found = this.As<ICodeFile>().AttachTypesSynchronously(rules, assembly, services, containingNamespace);
+        return Task.FromResult(found);
     }
 
     bool ICodeFile.AttachTypesSynchronously(GenerationRules rules, Assembly assembly, IServiceProvider services,
         string containingNamespace)
     {
-        throw new NotImplementedException();
+        _handlerType = assembly.ExportedTypes.FirstOrDefault(x => x.Name == _generatedType.TypeName);
+
+        if (_handlerType == null)
+        {
+            return false;
+        }
+        
+        // TODO -- create the actual handler
+
+        return true;
     }
 
     string ICodeFile.FileName => _fileName;
