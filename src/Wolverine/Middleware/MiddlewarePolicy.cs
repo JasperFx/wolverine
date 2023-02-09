df2295c962ee631c250f049dd5a677ec7d7ae6a4
Jeremy D. Miller <Jeremy.Miller@medeanalytics.com>
2023-02-09 09:54:42 -0600

    Upgraded JasperFx.CodeGeneration and removed now superfluous items. Closes GH-182

--- src/Wolverine/Middleware/MiddlewarePolicy.cs
+++ src/Wolverine/Middleware/MiddlewarePolicy.cs
@@ -9,67 +9,6 @@ using Wolverine.Runtime.Handlers;
 
 namespace Wolverine.Middleware;
 
-
-public class TryFinallyWrapperFrame : Frame
-{
-    private readonly Frame _inner;
-    private readonly Frame[] _finallys;
-
-    public TryFinallyWrapperFrame(Frame inner, Frame[] finallys) : base(inner.IsAsync)
-    {
-        _inner = inner;
-        _finallys = finallys;
-    }
-
-    public override void GenerateCode(GeneratedMethod method, ISourceWriter writer)
-    {
-        _inner.GenerateCode(method, writer);
-        writer.Write("BLOCK:try");
-        
-        Next?.GenerateCode(method, writer);
-        
-        writer.FinishBlock();
-        writer.Write("BLOCK:finally");
-        
-        if (_finallys.Length > 1)
-        {
-            for (var i = 1; i < _finallys.Length; i++)
-            {
-                _finallys[i - 1].Next = _finallys[i];
-            }
-        }
-        
-        _finallys[0].GenerateCode(method, writer);
-        
-        writer.FinishBlock();
-    }
-
-    public override IEnumerable<Variable> FindVariables(IMethodVariables chain)
-    {
-        foreach (var variable in _inner.FindVariables(chain))
-        {
-            yield return variable;
-        }
-
-        // NOT letting the finallys get involved with ordering frames
-        // because that way lies madness
-        foreach (var @finally in _finallys)
-        {
-            // Forcing it to evaluate and attach all variables
-            @finally.FindVariables(chain).ToArray();
-        }
-    }
-}
-
-// TODO -- move to JasperFx.CodeGeneration
-public static class FrameExtensions
-{
-    public static bool CreatesNewOf<T>(this MethodCall frame)
-    {
-        return frame.ReturnVariable?.VariableType == typeof(T) || frame.Creates.Any(x => x.VariableType == typeof(T));
-    }
-}
-
 internal class MiddlewarePolicy : IChainPolicy
 {
     public static readonly string[] BeforeMethodNames = { "Before", "BeforeAsync", "Load", "LoadAsync" };
