cfb12262e0d2300344450a5a9147fd0ba4281cd8
Jeremy D. Miller <jeremydmiller@yahoo.com>
2023-02-20 08:54:32 +0000

    Skeleton app and test harness for DiagnosticsApp for later

---
+++ src/Samples/Diagnostics/DiagnosticsApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using IntegrationTests;
+using Marten;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Oakton;
+using Oakton.Resources;
+using Wolverine;
+using Wolverine.Http;
+using Wolverine.Marten;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(Servers.PostgresConnectionString);
+    opts.DatabaseSchemaName = "http";
+}).IntegrateWithWolverine();
+
+
+
+builder.Services.AddResourceSetupOnStartup();
+
+// Need this.
+builder.Host.UseWolverine(opts =>
+{
+    opts.Policies.AutoApplyTransactions();
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseAuthorization();
+
+app.MapWolverineEndpoints(opts =>
+{
+
+});
+
+await app.RunOaktonCommands(args);
---
+++ src/Samples/Diagnostics/DiagnosticsTests/IntegrationContext.cs
@@ -0,0 +1,72 @@
+using Alba;
+using Marten;
+using Microsoft.Extensions.DependencyInjection;
+using Oakton;
+using Wolverine.Runtime;
+
+namespace DiagnosticsTests;
+
+public class AppFixture : IAsyncLifetime
+{
+    public async Task InitializeAsync()
+    {
+        OaktonEnvironment.AutoStartHost = true;
+        
+        // This is bootstrapping the actual application using
+        // its implied Program.Main() set up
+        Host = await AlbaHost.For<Program>(x => { });
+    }
+    
+    public IAlbaHost? Host { get; private set; }
+
+    public Task DisposeAsync()
+    {
+        return Host!.StopAsync();
+    }
+}
+
+[CollectionDefinition("integration")]
+public class IntegrationCollection : ICollectionFixture<AppFixture>
+{
+    
+}
+
+[Collection("integration")]
+public abstract class IntegrationContext : IAsyncLifetime
+{
+    private readonly AppFixture _fixture;
+
+    protected IntegrationContext(AppFixture fixture)
+    {
+        _fixture = fixture;
+        Runtime = (WolverineRuntime)fixture.Host!.Services.GetRequiredService<IWolverineRuntime>();
+    }
+
+    public Task<IScenarioResult> Scenario(Action<Scenario> configure)
+    {
+        return Host.Scenario(configure);
+    }
+
+    public WolverineRuntime Runtime { get; }
+    
+    public IAlbaHost Host => _fixture.Host!;
+    public IDocumentStore Store => _fixture.Host!.Services.GetRequiredService<IDocumentStore>();
+    
+    
+    async Task IAsyncLifetime.InitializeAsync()
+    {
+        // Using Marten, wipe out all data and reset the state
+        // back to exactly what we described in InitialAccountData
+        await Store.Advanced.ResetAllData();
+    }
+
+    // This is required because of the IAsyncLifetime 
+    // interface. Note that I do *not* tear down database
+    // state after the test. That's purposeful
+    public Task DisposeAsync()
+    {
+        return Task.CompletedTask;
+    }
+    
+
+}
