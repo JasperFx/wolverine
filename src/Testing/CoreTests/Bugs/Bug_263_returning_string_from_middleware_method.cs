785e66da70cde0a8cc32ffcf2e8b37002e44c12a
Jeremy D. Miller <Jeremy.Miller@medeanalytics.com>
2023-03-23 08:29:10 -0500

    LIttle codegen fixes. Closes GH-267

---
+++ src/Testing/CoreTests/Bugs/Bug_263_returning_string_from_middleware_method.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Hosting;
+using Xunit;
+
+namespace CoreTests.Bugs;
+
+public class Bug_263_returning_string_from_middleware_method
+{
+    [Fact]
+    public async Task can_return_and_use_string_from_tuple()
+    {
+        using var host = await Host.CreateDefaultBuilder()
+            .UseWolverine().StartAsync();
+
+        await host.InvokeAsync(new Bug263("Tom"));
+        
+        TupleHandler.Received.ShouldBe("Tom:1");
+    }
+}
+
+public record Bug263(string Name);
+
+public class TupleHandler
+{
+    public (string, Context) Load(Bug263 command)
+    {
+        return ($"{command.Name}:1", new Context());
+    }
+
+    public void Handle(Bug263 command, string text, Context context)
+    {
+        Received = text;
+        context.ShouldNotBeNull();
+    }
+
+    public static string Received { get; set; }
+}
+
+public class Context{}
