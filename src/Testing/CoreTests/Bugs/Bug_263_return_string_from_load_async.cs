da88e3c129957649f9168129324e0240f6d3afb9
Jeremy D. Miller <Jeremy.Miller@medeanalytics.com>
2023-03-27 08:34:13 -0500

    Test to verify that you can use strings as a return value in middleware. Closes GH-263

---
+++ src/Testing/CoreTests/Bugs/Bug_263_return_string_from_load_async.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Hosting;
+using Wolverine.Tracking;
+using Xunit;
+
+namespace CoreTests.Bugs;
+
+public class Bug_263_return_string_from_load_async
+{
+    [Fact]
+    public async Task can_return_a_string_from_a_load_precursor_and_pass_to_main_handle()
+    {
+        using var host = await Host.CreateDefaultBuilder()
+            .UseWolverine().StartAsync();
+
+        await host.InvokeMessageAndWaitAsync(new StringUsingCommand("one"));
+        
+        StringUsingCommandHandler.Recorded.ShouldBe("one:FromLoad");
+    }
+}
+
+public record StringUsingCommand(string Name);
+
+public class StringUsingCommandHandler
+{
+    public string Load(StringUsingCommand command)
+    {
+        return "FromLoad";
+    }
+
+    public void Handle(StringUsingCommand command, string text)
+    {
+        Recorded = $"{command.Name}:{text}";
+    }
+
+    public static string Recorded { get; set; }
+}
