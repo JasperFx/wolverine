9bd0a80f08a9c87a0d7693f25ef75a2396eae288
Jeremy D. Miller <Jeremy.Miller@medeanalytics.com>
2023-04-03 08:46:00 -0500

    Making Wolverine friendlier to generic types with more than 1 argument. Closes GH-229

---
+++ src/Testing/CoreTests/Bugs/Bug_229_using_generic_types_with_local_queue_names.cs
@@ -0,0 +1,35 @@
+using Microsoft.Extensions.Hosting;
+using Xunit;
+
+namespace CoreTests.Bugs;
+
+public class Bug_229_using_generic_types_with_local_queue_names
+{
+    [Fact]
+    public async Task can_start_up_the_app_without_invalid_queueNames_for_message_types()
+    {
+        using var host = await Host.CreateDefaultBuilder()
+            .UseWolverine()
+            .StartAsync();
+    }
+}
+
+public class TestSaga : Wolverine.Saga
+{
+    public Guid Id { get; set; }
+    public void Start(Result<Something, Error> resultFromAHandler) {}
+}
+public static class TestHandler
+{
+    public static void Handle(Result<Something, Error> cascadingMessageFromAnotherHandler)
+    {
+    }
+}
+
+public class Result<T1, T2>
+{
+    public Guid Id { get; set; }
+}
+
+public class Something{}
+public class Error{}
