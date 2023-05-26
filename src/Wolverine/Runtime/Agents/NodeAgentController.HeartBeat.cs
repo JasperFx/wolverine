6a896b5b2c0e25728ab9c89eac1319f97954a98a
Jeremy D. Miller <jeremydmiller@yahoo.com>
2023-05-26 11:39:27 -0500

    Tests for verifying assignments that successfully restarts agents

--- src/Wolverine/Runtime/Agents/NodeAgentController.HeartBeat.cs
+++ src/Wolverine/Runtime/Agents/NodeAgentController.HeartBeat.cs
@@ -7,7 +7,7 @@ public record CheckAgentHealth : IInternalMessage;
 public record VerifyAssignments : IInternalMessage;
 
 
-public partial class NodeAgentController : IInternalHandler<CheckAgentHealth>, IInternalHandler<VerifyAssignments>
+public partial class NodeAgentController : IInternalHandler<CheckAgentHealth>
 {
     private DateTimeOffset? _lastAssignmentCheck;
     
@@ -70,43 +70,6 @@ public partial class NodeAgentController : IInternalHandler<CheckAgentHealth>, I
         }
     }
     
-    public async IAsyncEnumerable<object> HandleAsync(VerifyAssignments message)
-    {
-        if (LastAssignments == null) yield break;
-        
-        var dict = new Dictionary<Uri, Guid>();
 
-        var requests = _tracker.OtherNodes()
-            .Select(node => _runtime.Agents.InvokeAsync<RunningAgents>(node.Id, new QueryAgents())).ToArray();
-        
-        // Loop and find. 
-        foreach (var request in requests)
-        {
-            var result = await request;
-            foreach (var agentUri in result.Agents)
-            {
-                dict[agentUri] = result.NodeId;
-            }
-        }
-
-        var delta = LastAssignments.FindDelta(dict);
-
-        foreach (var command in delta)
-        {
-            yield return command;
-        }
-        
-        _lastAssignmentCheck = DateTimeOffset.UtcNow;
-    }
 }
 
-internal class QueryAgents : IAgentCommand
-{
-    public async IAsyncEnumerable<object> ExecuteAsync(IWolverineRuntime runtime, [EnumeratorCancellation] CancellationToken cancellationToken)
-    {
-        var agents = runtime.Agents.AllRunningAgentUris();
-        yield return new RunningAgents(runtime.Options.UniqueNodeId, agents);
-    }
-}
-
-internal record RunningAgents(Guid NodeId, Uri[] Agents);
