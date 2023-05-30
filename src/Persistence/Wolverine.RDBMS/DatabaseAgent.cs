b6554274e8911aa78fdfdac1572b0e1204f50e49
Jeremy D. Miller <jeremydmiller@yahoo.com>
2023-05-30 09:49:50 -0500

    Running the database batches sequentially

--- src/Persistence/Wolverine.RDBMS/DatabaseAgent.cs
+++ src/Persistence/Wolverine.RDBMS/DatabaseAgent.cs
@@ -1,4 +1,7 @@
+using System.Threading.Tasks.Dataflow;
+using Microsoft.Extensions.Logging;
 using Wolverine.RDBMS.Durability;
+using Wolverine.RDBMS.Polling;
 using Wolverine.Runtime;
 using Wolverine.Runtime.Agents;
 using Wolverine.Runtime.WorkerQueues;
@@ -15,6 +18,7 @@ internal class DatabaseAgent : IAgent
     private readonly ILocalQueue _localQueue;
     private readonly DurabilitySettings _settings;
     private Timer? _scheduledJobTimer;
+    private readonly ActionBlock<DatabaseOperationBatch> _runningBlock;
 
     public DatabaseAgent(string databaseName, IWolverineRuntime runtime, IMessageDatabase database, DatabaseSettings databaseSettings)
     {
@@ -24,13 +28,43 @@ internal class DatabaseAgent : IAgent
         _settings = runtime.DurabilitySettings;
 
         Uri = new Uri($"{AgentScheme}://{databaseName}");
+
+        var invoker = runtime.FindInvoker(typeof(DatabaseOperationBatch));
+
+        var logger = runtime.LoggerFactory.CreateLogger<DatabaseAgent>();
+
+        _runningBlock = new ActionBlock<DatabaseOperationBatch>(async batch =>
+        {
+            if (runtime.Cancellation.IsCancellationRequested) return;
+            
+            try
+            {
+                await invoker.InvokeAsync(batch, new MessageBus(runtime));
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e, "Error trying to run durability agent commands");
+            }
+        }, new ExecutionDataflowBlockOptions
+        {
+            EnsureOrdered = true,
+            MaxDegreeOfParallelism = 1,
+            CancellationToken = runtime.Cancellation
+        });
     }
 
     public Task StartAsync(CancellationToken cancellationToken)
     {
         _scheduledJobTimer = new Timer(_ =>
         {
-            _database.EnqueueAsync(new RunScheduledMessagesOperation(_database, _settings, _localQueue)).GetAwaiter().GetResult();
+            var operations = new IDatabaseOperation[]
+            {
+                new RunScheduledMessagesOperation(_database, _settings, _localQueue)
+            };
+            
+            var batch = new DatabaseOperationBatch(_database, operations);
+            _runningBlock.Post(batch);
+
         }, _settings, _settings.ScheduledJobFirstExecution, _settings.ScheduledJobPollingTime);
         
         return Task.CompletedTask;
@@ -38,6 +72,8 @@ internal class DatabaseAgent : IAgent
 
     public async Task StopAsync(CancellationToken cancellationToken)
     {
+        _runningBlock.Complete();
+        
         if (_scheduledJobTimer != null)
         {
             await _scheduledJobTimer.DisposeAsync();
