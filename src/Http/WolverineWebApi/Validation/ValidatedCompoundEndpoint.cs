60e0bdff42756174bd4364dedc045d2d94b5cecc
wsuwala <wojciech.suwala@altkomsoftware.pl>
2023-07-29 12:58:28 +0200

    Middleware ordering problem with compound handlers fix. Moving fluent validation frames at zero index inserts validation code before Load/LoadAsync and [WolverineBefore] code

---
+++ src/Http/WolverineWebApi/Validation/ValidatedCompoundEndpoint.cs
@@ -0,0 +1,59 @@
+using FluentValidation;
+using Wolverine.Attributes;
+using Wolverine.Http;
+
+namespace WolverineWebApi.Validation;
+
+public class ValidatedCompoundEndpoint
+{
+    public static User? Load(BlockUser cmd)
+    {
+        if (cmd.UserId == null)
+            throw new ApplicationException("Something went wrong. Fluent validation should stop execution before load");
+
+        return new User(cmd.UserId);
+    }
+    
+    [WolverineBefore]
+    public static IResult Validate(User user)
+    {
+        if (user == null)
+            throw new ApplicationException("Something went wrong. Fluent validation should stop execution before load");
+
+        return WolverineContinue.Result();
+    }
+    
+    [WolverineDelete("/validate/user-compound")]
+    public static  string Handle(BlockUser cmd, User user)
+    {
+        return "Ok - user blocked";
+    }
+  
+}
+
+public record BlockUser(string? UserId);
+
+public class BlockUserValidator : AbstractValidator<BlockUser>
+{
+    public BlockUserValidator()
+    {
+        RuleFor(c => c.UserId).NotEmpty();
+    }
+}
+
+public class User
+{
+    public string Id { get; private set; }
+    public bool IsBlocked { get; private set; }
+    
+    public User(string id)
+    {
+        Id = id;
+        IsBlocked = false;
+    }
+
+    public void Block()
+    {
+        IsBlocked = true;
+    }
+}
