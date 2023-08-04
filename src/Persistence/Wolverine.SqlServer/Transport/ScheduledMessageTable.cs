03f2930f1bd84669d1612b14c6fedc25b01ba29e
Jeremy D. Miller <jeremydmiller@yahoo.com>
2023-08-04 16:16:08 -0500

    Added index to the Scheduled job table

--- src/Persistence/Wolverine.SqlServer/Transport/ScheduledMessageTable.cs
+++ src/Persistence/Wolverine.SqlServer/Transport/ScheduledMessageTable.cs
@@ -12,8 +12,14 @@ internal class ScheduledMessageTable : Table
         AddColumn<Guid>(DatabaseConstants.Id).AsPrimaryKey();
         AddColumn(DatabaseConstants.Body, "varbinary(max)").NotNull();
         AddColumn(DatabaseConstants.MessageType, "varchar(250)").NotNull();
-        AddColumn<DateTimeOffset>(DatabaseConstants.ExecutionTime).AllowNulls();
+        AddColumn<DateTimeOffset>(DatabaseConstants.ExecutionTime).NotNull();
         AddColumn<DateTimeOffset>(DatabaseConstants.KeepUntil);
         AddColumn<DateTimeOffset>("timestamp").DefaultValueByExpression("SYSDATETIMEOFFSET()");
+        
+        // Definitely want to index the execution time. Far more reads than writes. We think. 
+        Indexes.Add(new IndexDefinition($"idx_{tableName}_execution_time")
+        {
+            Columns = new string[]{DatabaseConstants.ExecutionTime}
+        });
     }
 }
