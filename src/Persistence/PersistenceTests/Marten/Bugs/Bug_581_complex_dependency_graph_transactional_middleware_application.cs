b87a73d5b5103c5a3ff8f5c166ad17bc6daa20f5
Jeremy D. Miller <jeremydmiller@yahoo.com>
2023-10-09 11:48:12 -0500

    Test that proves that the marten transactional detection works for constructor dependencies of the handler itself. Closes GH-584

---
+++ src/Persistence/PersistenceTests/Marten/Bugs/Bug_581_complex_dependency_graph_transactional_middleware_application.cs
@@ -0,0 +1,122 @@
+using IntegrationTests;
+using JasperFx.Core.Reflection;
+using Lamar;
+using Marten;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Identity.Client;
+using Shouldly;
+using Wolverine;
+using Wolverine.Marten;
+using Wolverine.Marten.Persistence.Sagas;
+using Wolverine.Runtime.Handlers;
+using Wolverine.Tracking;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace PersistenceTests.Marten.Bugs;
+
+public class Bug_581_complex_dependency_graph_transactional_middleware_application : PostgresqlContext
+{
+    private readonly ITestOutputHelper _output;
+
+    public Bug_581_complex_dependency_graph_transactional_middleware_application(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+
+    [Fact]
+    public async Task apply_transactional_middleware_when_session_is_used_internally_in_dependency_tree()
+    {
+        using var host = await Host.CreateDefaultBuilder()
+            .UseWolverine(opts =>
+            {
+                opts.Services.AddMarten(m =>
+                {
+                    m.Connection(Servers.PostgresConnectionString);
+                    m.DatabaseSchemaName = "users";
+                }).IntegrateWithWolverine();
+
+                opts.Services.AddScoped<IUserService, UserService>();
+                opts.Services.AddScoped<IUserRepository, UserRepository>();
+                
+                opts.Policies.AutoApplyTransactions();
+            }).StartAsync();
+
+        var runtime = host.GetRuntime();
+        var handlers = runtime.Handlers;
+
+        var handler = handlers.HandlerFor<CreateUser>().As<MessageHandler>();
+        
+        new MartenPersistenceFrameProvider()
+            .CanApply(handler.Chain, (IContainer)host.Services).ShouldBeTrue();
+
+        var code = handler.Chain.SourceCode;
+        _output.WriteLine(code);
+    }
+}
+
+public class CreateUserHandler
+{
+    private readonly IUserService _service;
+
+    public CreateUserHandler(IUserService service)
+    {
+        _service = service;
+    }
+
+    public async Task Handle(CreateUser command)
+    {
+        await _service.CreateUser(command.Name);
+    }
+}
+
+public record CreateUser(string Name);
+
+public interface IUserService
+{
+    Task CreateUser(string name);
+}
+
+public class UserService : IUserService
+{
+    private readonly IUserRepository _repository;
+
+    public UserService(IUserRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public Task CreateUser(string name)
+    {
+        var user = new User { Name = name };
+        return _repository.Store(user);
+    }
+}
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+}
+
+public interface IUserRepository
+{
+    Task Store(User user);
+}
+
+public class UserRepository : IUserRepository
+{
+    private readonly IDocumentSession _session;
+
+    public UserRepository(IDocumentSession session)
+    {
+        _session = session;
+    }
+
+    public async Task Store(User user)
+    {
+        _session.Store(user);
+        await _session.SaveChangesAsync();
+    }
+}
