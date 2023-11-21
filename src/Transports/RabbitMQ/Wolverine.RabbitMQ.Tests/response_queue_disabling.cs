2a1cf428fd5deaaf9d17a5f951d0f582875c8fe9
vitalie.glinca <vitalie.glinca@maib.md>
2023-11-21 15:21:56 +0200

    Add tests

---
+++ src/Transports/RabbitMQ/Wolverine.RabbitMQ.Tests/response_queue_disabling.cs
@@ -0,0 +1,33 @@
+using Microsoft.Extensions.Hosting;
+using Shouldly;
+using Xunit;
+
+namespace Wolverine.RabbitMQ.Tests;
+
+public class response_queue_disabling : IAsyncLifetime
+{
+    private IHost _host;
+    
+    [Fact]
+    public void reply_queue_should_not_be_declared()
+    {
+        var transport = _host.Get<WolverineOptions>().RabbitMqTransport();
+        
+        transport.ReplyEndpoint()
+            .ShouldBeNull();
+    }
+
+    public async Task InitializeAsync()
+    {
+        _host = await Host.CreateDefaultBuilder()
+            .UseWolverine(opts =>
+            {
+                opts.ServiceName = "MyApp";
+                opts.UseRabbitMq()
+                    .DisableSystemRequestReplyQueueDeclaration();
+            }).StartAsync();
+    }
+    
+    public Task DisposeAsync() => _host.StopAsync();
+}
+ 
