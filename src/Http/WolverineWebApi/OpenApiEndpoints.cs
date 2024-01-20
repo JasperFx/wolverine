60197d797c065be74a06c90ec2845dce6ca5f789
JT <Hawxy@users.noreply.github.com>
2024-01-20 20:03:14 +0800

    Fix ProducesResponseType support and improve accuracy of annotations

--- src/Http/WolverineWebApi/OpenApiEndpoints.cs
+++ src/Http/WolverineWebApi/OpenApiEndpoints.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.OpenApi.Models;
 using Wolverine;
 using Wolverine.Http;
@@ -32,6 +33,22 @@ public class OpenApiEndpoints
     {
         return new Reservation();
     }
+    
+    [ExpectMatch(OperationType.Get, "/expect/getresult")]
+    [ProducesResponseType(typeof(ProblemDetails), 400)]
+    [ProducesResponseType(typeof(string), 200)]
+    [WolverineGet("/openapi/getresult")]
+    public IResult GetResult()
+    {
+        return Results.Ok("a string");
+    }
+
+    [ExpectMatch(OperationType.Get, "/expect/getresulttyped")]
+    [WolverineGet("/openapi/getresulttyped")]
+    public Results<Ok<Reservation>, ProblemHttpResult> GetResultTyped()
+    {
+        return TypedResults.Ok(new Reservation());
+    }
 
     [ExpectMatch(OperationType.Get, "/expect/nocontent")]
     [WolverineGet("/openapi/nocontent")]
@@ -54,6 +71,8 @@ public class OpenApiEndpoints
         app.MapPost("/expect/nocontent", () => TypedResults.NoContent());
         app.MapGet("/expect/string", () => "hello, world");
         app.MapGet("/expect/json", () => TypedResults.Json(new Reservation())).Produces(404);
+        app.MapGet("/expect/getresult",  () => Results.Ok("a string")).ProducesProblem(400).Produces(404).Produces<string>(contentType: "text/plain");
+        app.MapGet("/expect/getresulttyped", Results<Ok<Reservation>, ProblemHttpResult> () => TypedResults.Ok(new Reservation()));
     }
 }
 
