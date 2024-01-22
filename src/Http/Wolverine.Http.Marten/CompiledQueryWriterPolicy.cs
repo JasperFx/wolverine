d33808b01b9cc6f65d9d7445cdbfb87b427c40ca
Felix Winterhalter <felix.winterhalter@fau.de>
2024-01-22 17:47:27 +0100

    refactor: Refactor existing CompiledQuery policy

--- src/Http/Wolverine.Http.Marten/CompiledQueryWriterPolicy.cs
+++ src/Http/Wolverine.Http.Marten/CompiledQueryWriterPolicy.cs
@@ -1,7 +1,9 @@
 using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
+using System.Security.AccessControl;
 using JasperFx.CodeGeneration.Frames;
 using JasperFx.CodeGeneration.Model;
+using JasperFx.Core.Reflection;
 using JasperFx.RuntimeCompiler;
 using Marten.Linq;
 using Wolverine.Http.Resources;
@@ -13,67 +15,50 @@ namespace Wolverine.Http.Marten;
 
 public class CompiledQueryWriterPolicy : IResourceWriterPolicy
 {
+    private readonly string _responseType;
+    private readonly int _successStatusCode;
+
+    public CompiledQueryWriterPolicy(string responseType = "application/json", int successStatusCode = 200)
+    {
+        _responseType = responseType;
+        _successStatusCode = successStatusCode;
+    }
+    
     public bool TryApply(HttpChain chain)
     {
-        bool ImplementsGenericInterface(Type type, Type assigned, [NotNullWhen(true)] out Type[]? types)
-        {
-            types = null;
-            var result = type.GetInterfaces().FirstOrDefault(x =>
-                x.IsGenericType && x.GetGenericTypeDefinition() == assigned);
-            if (result is null) return false;
-            types = result.GetGenericArguments();
-            return true;
-        }
-
         var result = chain.Method.Creates.FirstOrDefault();
         if (result is null) return false;
+
         // Are we dealing with a compiled query or not?
-        if (ImplementsGenericInterface(result.VariableType, typeof(ICompiledQuery<,>), out var arguments))
-        {
-            MethodInfo methodToCall;
-            
-            // Is it a compiled list query? 
-            if (ImplementsGenericInterface(result.VariableType, typeof(ICompiledListQuery<,>), out _))
-            {
-                // Cannot use GetMethod here because its ambiguous, can't use GetMethod with type parameters because of
-                // WriteArray being a generic method where binding on types passed doesn't work.
-                var method = typeof(QueryableExtensions).GetMethods().Where(x =>
-                    x is { Name: nameof(QueryableExtensions.WriteArray), IsGenericMethod: true } &&
-                    x.GetGenericArguments().Length == 2).ToArray();
-                if (method.Length != 1)
-                {
-                    throw new CodeGenerationException(method, new Exception(
-                        "Cannot find correct method for Marten.AspNetCore.QueryableExtensions.WriteArray to bind to. Incompatible Versions?"));
-                }
+        var compiledQueryClosure = result.VariableType.FindInterfaceThatCloses(typeof(ICompiledQuery<,>));
+        if (compiledQueryClosure is null) return false;
 
-                methodToCall = method[0].MakeGenericMethod(arguments);
-            }
-            else
-            {
-                var method = typeof(QueryableExtensions).GetMethod(nameof(QueryableExtensions.WriteOne));
-                if (method is null)
-                {
-                    throw new CodeGenerationException(typeof(QueryableExtensions), new Exception(
-                        "Cannot find correct method for Marten.AspNetCore.QueryableExtensions.WriteOne to bind to. Incompatible Versions?"));
-                }
+        var arguments = result.VariableType.FindInterfaceThatCloses(typeof(ICompiledQuery<,>))!.GetGenericArguments();
 
-                methodToCall = method.MakeGenericMethod(arguments);
-            }
+        var methodCall = typeof(MartenWriteArrayMethodCall<,>).CloseAndBuildAs<MethodCall>(result, (_responseType, _successStatusCode), arguments);
 
+        chain.Postprocessors.Add(methodCall);
+        return true;
+    }
+}
 
-            var call = new MethodCall(typeof(QueryableExtensions), methodToCall)
-                            {
-                                Arguments =
-                                {
-                                    [1] = result,
-                                    [3] = new StaticVariable(typeof(string), "\"application/json\""),
-                                    [4] = new StaticVariable(typeof(int), "200")
-                                }
-                            };
-                            chain.Postprocessors.Add(call);
-                            return true;
-        }
-        return false;
+public class MartenWriteArrayMethodCall<TDoc, TOut> : MethodCall
+{
+    public MartenWriteArrayMethodCall(Variable resultVariable, (string responseType, int successStatusCode) options) : base(typeof(QueryableExtensions), FindMethod(resultVariable))
+    {
+        Arguments[1] = resultVariable;
+        Arguments[3] = Constant.ForString(options.responseType);
+        Arguments[4] = Constant.For(options.successStatusCode);
+    }
+
+    static MethodInfo FindMethod(Variable resultVariable)
+    {
+        // If we are dealing with a list type we want WriteArray, otherwise we want WriteOne
+        return resultVariable.VariableType.Closes(typeof(ICompiledListQuery<,>))
+            ? ReflectionHelper.GetMethod((string x) =>
+                QueryableExtensions.WriteArray(null!, (ICompiledQuery<TDoc, TOut>)null!, null!, null!, 0))!
+            : ReflectionHelper.GetMethod((string x) =>
+                QueryableExtensions.WriteOne(null!, (ICompiledQuery<TDoc, TOut>)null!, null!, null!, 0))!;
     }
 }
 #nullable disable
