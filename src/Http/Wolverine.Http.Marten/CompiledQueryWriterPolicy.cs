f1c62cccee615a83efd0f9333e6eb40a75a4dc76
Felix Winterhalter <felix.winterhalter@fau.de>
2024-01-25 14:54:20 +0100

    feat: Add primitive handling to CompiledQuery result writer

--- src/Http/Wolverine.Http.Marten/CompiledQueryWriterPolicy.cs
+++ src/Http/Wolverine.Http.Marten/CompiledQueryWriterPolicy.cs
@@ -2,6 +2,7 @@ using System.Reflection;
 using JasperFx.CodeGeneration.Frames;
 using JasperFx.CodeGeneration.Model;
 using JasperFx.Core.Reflection;
+using Marten;
 using Marten.Linq;
 using Wolverine.Http.Resources;
 using QueryableExtensions = Marten.AspNetCore.QueryableExtensions;
@@ -31,17 +32,51 @@ public class CompiledQueryWriterPolicy : IResourceWriterPolicy
         if (compiledQueryClosure is null) return false;
 
         var arguments = compiledQueryClosure.GetGenericArguments();
+        
+        // If we're dealing with a primitive return type we need to write its string representation directly
+        if (arguments[1].IsPrimitive)
+        {
+            // This call runs the query
+            var queryCall =
+                typeof(MartenQueryMethodCall<,>).CloseAndBuildAs<MethodCall>(result, arguments);
+            chain.Postprocessors.Add(queryCall);
 
-        var methodCall = typeof(MartenWriteArrayMethodCall<,>).CloseAndBuildAs<MethodCall>(result, (_responseType, _successStatusCode), arguments);
+            // This call writes the response directly to the HttpContext as a string
+            var writeStringCall = MethodCall.For<HttpHandler>(handler => HttpHandler.WriteString(null!, ""));
+            writeStringCall.Arguments[1] = new Variable(queryCall.ReturnVariable!.VariableType,
+                $"{queryCall.ReturnVariable.Usage}.ToString()", queryCall);
+            chain.Postprocessors.Add(writeStringCall);
+        }
+        else
+        {
 
-        chain.Postprocessors.Add(methodCall);
+            var writeJsonCall =
+                typeof(MartenWriteJsonToStreamMethodCall<,>).CloseAndBuildAs<MethodCall>(result,
+                    (_responseType, _successStatusCode), arguments);
+            chain.Postprocessors.Add(writeJsonCall);
+        }
+
         return true;
     }
 }
 
-public class MartenWriteArrayMethodCall<TDoc, TOut> : MethodCall
+public class MartenQueryMethodCall<TDoc, TOut> : MethodCall
 {
-    public MartenWriteArrayMethodCall(Variable resultVariable, (string responseType, int successStatusCode) options) : base(typeof(QueryableExtensions), FindMethod(resultVariable))
+    public MartenQueryMethodCall(Variable resultVariable) : base(typeof(IDocumentSession), FindMethod())
+    {
+        Arguments[0] = resultVariable;
+    }
+
+    static MethodInfo FindMethod()
+    {
+        return ReflectionHelper.GetMethod<IDocumentSession>(x =>
+            x.QueryAsync((ICompiledQuery<TDoc, TOut>)null!, default))!;
+    }
+}
+
+public class MartenWriteJsonToStreamMethodCall<TDoc, TOut> : MethodCall
+{
+    public MartenWriteJsonToStreamMethodCall(Variable resultVariable, (string responseType, int successStatusCode) options) : base(typeof(QueryableExtensions), FindMethod(resultVariable))
     {
         Arguments[1] = resultVariable;
         Arguments[3] = Constant.ForString(options.responseType);
--- src/Http/Wolverine.Http.Tests/Marten/compiled_query_writer.cs
+++ src/Http/Wolverine.Http.Tests/Marten/compiled_query_writer.cs
@@ -43,6 +43,28 @@ public class compiled_query_writer : IntegrationContext
         approvedInvoiceList.ShouldNotBeNull();
         approvedInvoiceList.Count.ShouldBe(approvedInvoices);
     }
+    
+    [Fact]
+    public async Task endpoint_returning_compiled_primitive_query_should_return_query_result()
+    {
+        await using var session = Store.LightweightSession();
+        int invoicesCount = 5;
+        for (int i = 0; i < invoicesCount; i++)
+        {
+            var invoice =
+                new Invoice()
+                {
+                };
+            session.Store(invoice);
+        }
+
+        await session.SaveChangesAsync();
+
+        var approvedInvoiceList = await Host.GetAsText("/invoices/compiled/count");
+        approvedInvoiceList.ShouldNotBeNull();
+        int.TryParse(approvedInvoiceList, out var result).ShouldBeTrue();
+        result.ShouldBe(invoicesCount);
+    }
 
     [Fact]
     public async Task endpoint_returning_compiled_query_should_return_query_result()
