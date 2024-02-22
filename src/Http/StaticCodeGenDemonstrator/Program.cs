7fbb04e029bbff1bcccb81ae66fac815dd4167b6
Jeremy D. Miller <jeremydmiller@yahoo.com>
2024-02-21 19:40:00 -0600

    Demonstrator to test custom error action plus static code analysis. Closes GH-739

---
+++ src/Http/StaticCodeGenDemonstrator/Program.cs
@@ -0,0 +1,64 @@
+using JasperFx.CodeGeneration;
+using Oakton;
+using Wolverine;
+using Wolverine.ErrorHandling;
+using Wolverine.Http;
+using Wolverine.Runtime.Handlers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseWolverine(opts =>
+{
+    opts.CodeGeneration.TypeLoadMode = TypeLoadMode.Static;
+});
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapWolverineEndpoints();
+
+return await app.RunOaktonCommands(args);
+
+public static class TestEndpoint
+{
+    [WolverinePost("test")]
+    public static async Task Post(IMessageBus bus)
+    {
+        await bus.SendAsync(new BadMessage(1));
+    }
+}
+
+public sealed record BadMessage(int BadMessageId);
+
+public static class BadMessageHandler
+{
+    public static void Configure(HandlerChain chain)
+    {
+        chain.OnAnyException()
+            .MoveToErrorQueue()
+            .And((_, context, ex) =>
+            {
+                if (context.Envelope?.Message is BadMessage message)
+                {
+                    // do something
+                }
+                return ValueTask.CompletedTask;
+            });
+    }
+
+    public static void Handle(BadMessage message)
+    {
+        throw new Exception("Test!");
+    }
+}
