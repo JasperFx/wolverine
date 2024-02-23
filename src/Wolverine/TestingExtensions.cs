bd76653cf7e61fd75fb91a4af36fbffcb846c9a9
Jeremy D. Miller <jeremydmiller@yahoo.com>
2024-02-23 10:34:03 -0600

    First basic tests of distributing projections by node with a single tenant

--- src/Wolverine/TestingExtensions.cs
+++ src/Wolverine/TestingExtensions.cs
@@ -1,5 +1,8 @@
 using JasperFx.Core;
 using JasperFx.Core.Reflection;
+using Microsoft.Extensions.Hosting;
+using Wolverine.Logging;
+using Wolverine.Tracking;
 
 namespace Wolverine;
 
@@ -168,4 +171,93 @@ public static class TestingExtensions
             $"Unable to find an envelope for type {typeof(T).FullNameInCode()}, actual messages were {messages.toListOfMessages()}",
             messages.ToArray());
     }
+    
+    /// <summary>
+    /// Test helper for agent assignment within Wolverine. Definitely an advanced usage!
+    /// </summary>
+    /// <param name="expectedLeader"></param>
+    /// <param name="configure"></param>
+    /// <param name="timeout"></param>
+    /// <returns></returns>
+    public static Task<bool> WaitUntilAssignmentsChangeTo(this IHost expectedLeader,
+        Action<AssignmentWaiter> configure, TimeSpan timeout)
+    {
+        var waiter = new AssignmentWaiter(expectedLeader);
+        configure(waiter);
+
+        return waiter.Start(timeout);
+    }
+    
+    public class AssignmentWaiter : IObserver<IWolverineEvent>
+    {
+        private readonly TaskCompletionSource<bool> _completion = new();
+    
+        private IDisposable _unsubscribe;
+        private readonly WolverineTracker _tracker;
+
+        public Dictionary<Guid, int> AgentCountByHost { get; } = new();
+        public string AgentScheme { get; set; }
+
+        public AssignmentWaiter(IHost leader)
+        {
+            _tracker = leader.GetRuntime().Tracker;
+        }
+
+        public void ExpectRunningAgents(IHost host, int runningCount)
+        {
+            var id = host.GetRuntime().Options.UniqueNodeId;
+            AgentCountByHost[id] = runningCount;
+        }
+
+        public Task<bool> Start(TimeSpan timeout)
+        {
+            if (HasReached()) return Task.FromResult(true);
+        
+            _unsubscribe = _tracker.Subscribe(this);
+        
+            var timeout1 = new CancellationTokenSource(timeout);
+            timeout1.Token.Register(() =>
+            {
+                _completion.TrySetException(new TimeoutException(
+                    "Did not reach the expected state or message in time"));
+            });
+
+
+            return _completion.Task;
+        }
+
+        public bool HasReached()
+        {
+            foreach (var pair in AgentCountByHost)
+            {
+                Func<Uri, bool> filter = AgentScheme.IsEmpty()
+                    ? x => !x.Scheme.StartsWith("wolverine")
+                    : x => x.Scheme.EqualsIgnoreCase(AgentScheme);
+            
+                var runningCount = _tracker.Agents.ToArray().Where(x => filter(x.Key)).Count(x => x.Value == pair.Key);
+                if (pair.Value != runningCount) return false;
+            }
+
+            return true;
+        }
+
+        public void OnCompleted()
+        {
+        }
+
+        public void OnError(Exception error)
+        {
+            _completion.SetException(error);
+        }
+
+        public void OnNext(IWolverineEvent value)
+        {
+            if (HasReached())
+            {
+                _completion.TrySetResult(true);
+                _unsubscribe.Dispose();
+            }
+        }
+    }
 }
+
