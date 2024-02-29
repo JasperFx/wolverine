2b74a1be2a7f7aa01808b14af63d78056283e91d
woksin <sindrewilting@gmail.com>
2024-02-29 21:23:12 +0100

    Test that proves the functionality

---
+++ src/Testing/CoreTests/Compilation/handler_with_optional_side_effect.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using TestingSupport;
+using Wolverine.Runtime.Handlers;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace CoreTests.Compilation;
+
+public class handler_with_optional_side_effect
+{
+    private readonly ITestOutputHelper _output;
+
+    public handler_with_optional_side_effect(ITestOutputHelper output)
+    {
+        _output = output;
+    }
+
+    [Fact]
+    public async Task can_compile_correctly_for_handler_with_optional_side_effect_returning_null()
+    {
+        using var host = WolverineHost.Basic();
+
+        var bus = host.Services.GetRequiredService<IMessageBus>();
+        await bus.InvokeAsync(new SomeCommand());
+
+        var graph = host.Services.GetRequiredService<HandlerGraph>();
+        var chain = graph.ChainFor<SomeCommand>();
+        
+        _output.WriteLine(chain.SourceCode);
+    }
+    
+    [Fact]
+    public async Task can_compile_correctly_for_handler_with_optional_side_effect_returning_the_side_effect()
+    {
+        using var host = WolverineHost.Basic();
+
+        var bus = host.Services.GetRequiredService<IMessageBus>();
+        await bus.InvokeAsync(new SomeOtherCommand());
+
+        var graph = host.Services.GetRequiredService<HandlerGraph>();
+        var chain = graph.ChainFor<SomeOtherCommand>();
+        
+        _output.WriteLine(chain.SourceCode);
+    }
+}
+
+public class SomeCommand
+{
+}
+
+public class SomeOtherCommand
+{
+}
+
+public class SomeSideEffect : ISideEffect
+{
+    public Task ExecuteAsync() => Task.CompletedTask;
+}
+
+public class SomeCommandHandler
+{
+    public SomeSideEffect? Handle(SomeCommand cmd)
+    {
+        return null;
+    }
+    
+    public SomeSideEffect? Handle(SomeOtherCommand cmd)
+    {
+        return new SomeSideEffect();
+    }
+}
