3bccc468b1ceea698fcb4e29c4730016fcfbd573
Jeremy D. Miller <jeremydmiller@yahoo.com>
2024-03-13 07:26:11 -0500

    Making FailureAcknowledgement be self serializable. Closes GH-768

---
+++ src/Testing/CoreTests/Runtime/RemoteInvocation/FailureAcknowledgementTests.cs
@@ -0,0 +1,25 @@
+using Wolverine.Runtime.RemoteInvocation;
+using Xunit;
+
+namespace CoreTests.Runtime.RemoteInvocation;
+
+public class FailureAcknowledgementTests
+{
+    [Fact]
+    public void serialize_FailureAcknowledgement()
+    {
+        var ack = new FailureAcknowledgement
+        {
+            Message = "Bad",
+            RequestId = Guid.NewGuid()
+        };
+
+        var bytes = ack.Write();
+
+        var ack2 = (FailureAcknowledgement)FailureAcknowledgement.Read(bytes);
+        
+        ack2.Message.ShouldBe(ack.Message);
+        ack2.RequestId.ShouldBe(ack.RequestId);
+
+    }
+}
