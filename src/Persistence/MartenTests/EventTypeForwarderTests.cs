bcb05a043dc04ca0471c7e3e10aa8259d90a930a
Jeremy D. Miller <jeremydmiller@yahoo.com>
2024-03-29 10:54:09 -0500

    new handler type forwarder for Marten events

---
+++ src/Persistence/MartenTests/EventTypeForwarderTests.cs
@@ -0,0 +1,59 @@
+using Marten.Events;
+using Shouldly;
+using Wolverine.Marten;
+
+namespace MartenTests;
+
+public class EventTypeForwarderTests
+{
+    [Fact]
+    public void positive_forward()
+    {
+        var messageType = typeof(FakeEvent<SecondEvent>);
+        var forwarder = new EventTypeForwarder();
+        forwarder.TryFindHandledType(messageType, out var actual)
+            .ShouldBeTrue();
+        
+        actual.ShouldBe(typeof(IEvent<SecondEvent>));
+    }
+
+    [Fact]
+    public void negative_forwarding()
+    {
+        var forwarder = new EventTypeForwarder();
+        forwarder.TryFindHandledType(typeof(SecondEvent), out var actual)
+            .ShouldBeFalse();
+    }
+}
+
+public class FakeEvent<T> : IEvent<T>
+{
+    public void SetHeader(string key, object value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public object GetHeader(string key)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Guid Id { get; set; }
+    public long Version { get; set; }
+    public long Sequence { get; set; }
+    object IEvent.Data => Data;
+
+    public T Data { get; }
+    public Guid StreamId { get; set; }
+    public string StreamKey { get; set; }
+    public DateTimeOffset Timestamp { get; set; }
+    public string TenantId { get; set; }
+    public Type EventType { get; }
+    public string EventTypeName { get; set; }
+    public string DotNetTypeName { get; set; }
+    public string CausationId { get; set; }
+    public string CorrelationId { get; set; }
+    public Dictionary<string, object> Headers { get; set; }
+    public bool IsArchived { get; set; }
+    public string AggregateTypeName { get; set; }
+}
