a72cde06a76941a61796fae54ab6ba773344dd0d
Jeremy D. Miller <jeremydmiller@yahoo.com>
2024-04-26 08:02:46 -0500

    extra tests for dynamic marten multi-tenancy

--- src/Persistence/MartenTests/MultiTenancy/dynamically_spin_up_new_durability_agents_for_new_tenant_databases.cs
+++ src/Persistence/MartenTests/MultiTenancy/dynamically_spin_up_new_durability_agents_for_new_tenant_databases.cs
@@ -5,11 +5,13 @@ using Marten.Storage;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Npgsql;
+using Shouldly;
 using Weasel.Postgresql;
 using Weasel.Postgresql.Migrations;
 using Wolverine;
 using Wolverine.Marten;
 using Wolverine.RDBMS;
+using Wolverine.Tracking;
 
 namespace MartenTests.MultiTenancy;
 
@@ -59,6 +61,9 @@ public class dynamically_spin_up_new_durability_agents_for_new_tenant_databases 
                 // This is too extreme for real usage, but helps tests to run faster
                 opts.Durability.NodeReassignmentPollingTime = 1.Seconds();
                 opts.Durability.HealthCheckPollingTime = 1.Seconds();
+                
+                opts.Policies.AutoApplyTransactions();
+                opts.Policies.UseDurableLocalQueues();
                 
                 opts.Services.AddMarten(o =>
                     {
@@ -114,6 +119,83 @@ public class dynamically_spin_up_new_durability_agents_for_new_tenant_databases 
             w.ExpectRunningAgents(_host, 4);
         }, 1.Minutes());
     }
-    
 
+    [Fact]
+    public async Task ability_to_execute_commands_immediately_on_new_tenant_databases_in_leader()
+    {
+        // Adding new tenant databases
+        var tenancy = (MasterTableTenancy)theStore.Options.Tenancy;
+        await tenancy.AddDatabaseRecordAsync("tenant1", tenant1ConnectionString);
+        await tenancy.AddDatabaseRecordAsync("tenant2", tenant2ConnectionString);
+
+        var command = new PersistDoc(Guid.NewGuid());
+
+        // Execute a command against the newly added tenant database
+        await _host.InvokeMessageAndWaitAsync(command, "tenant1");
+
+        var store = _host.Services.GetRequiredService<IDocumentStore>();
+        using var session = store.LightweightSession("tenant1");
+        var doc = await session.LoadAsync<PersistedDoc>(command.Id);
+
+        doc.ShouldNotBeNull();
+    }
+
+    [Fact]
+    public async Task ability_to_execute_commands_immediately_on_new_tenant_databases_in_non_leader()
+    {
+        // Setting up a Host with Multi-tenancy
+        using var otherHost = await Host.CreateDefaultBuilder()
+            .UseWolverine(opts =>
+            {
+                // This is too extreme for real usage, but helps tests to run faster
+                opts.Durability.NodeReassignmentPollingTime = 1.Seconds();
+                opts.Durability.HealthCheckPollingTime = 1.Seconds();
+                
+                opts.Policies.AutoApplyTransactions();
+                opts.Policies.UseDurableLocalQueues();
+                
+                opts.Services.AddMarten(o =>
+                    {
+                        // This is a new strategy for configuring tenant databases with Marten
+                        // In this usage, Marten is tracking the tenant databases in a single table in the "master"
+                        // database by tenant
+                        o.MultiTenantedDatabasesWithMasterDatabaseTable(Servers.PostgresConnectionString, "tenants");
+                    })
+                    .IntegrateWithWolverine("mt", masterDatabaseConnectionString:Servers.PostgresConnectionString)
+
+                    // All detected changes will be applied to all
+                    // the configured tenant databases on startup
+                    .ApplyAllDatabaseChangesOnStartup();
+            })
+            .StartAsync();
+        
+        var tenancy = (MasterTableTenancy)theStore.Options.Tenancy;
+        await tenancy.AddDatabaseRecordAsync("tenant1", tenant1ConnectionString);
+        await tenancy.AddDatabaseRecordAsync("tenant2", tenant2ConnectionString);
+
+        var command = new PersistDoc(Guid.NewGuid());
+
+        await otherHost.InvokeMessageAndWaitAsync(command, "tenant1");
+
+        var store = otherHost.Services.GetRequiredService<IDocumentStore>();
+        using var session = store.LightweightSession("tenant1");
+        var doc = await session.LoadAsync<PersistedDoc>(command.Id);
+
+        doc.ShouldNotBeNull();
+    }
+}
+
+public class PersistedDoc
+{
+    public Guid Id { get; set; }
+}
+
+public record PersistDoc(Guid Id);
+
+public static class PeristDocHandler
+{
+    public static IMartenOp Handle(PersistDoc command)
+    {
+        return MartenOps.Store(new PersistedDoc() { Id = command.Id });
+    }
 }
