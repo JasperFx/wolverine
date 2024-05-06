7b3bc142957f41bd9df2273ecfeb1ee9fd18f7ad
Simon Cropp <simon.cropp@gmail.com>
2024-05-06 20:17:23 +1000

    Update WolverineModuleAttributeT.cs

--- src/Wolverine/Attributes/WolverineModuleAttributeT.cs
+++ src/Wolverine/Attributes/WolverineModuleAttributeT.cs
@@ -1,8 +1,7 @@
 ﻿namespace Wolverine.Attributes;
 
 /// <summary>
-///     Marks the assembly as an automatically loaded Wolverine extension
-///     module
+///     Marks the assembly as an automatically loaded Wolverine extension module
 /// </summary>
 [AttributeUsage(AttributeTargets.Assembly)]
 public class WolverineModuleAttribute<T> : WolverineModuleAttribute
@@ -12,8 +11,6 @@ public class WolverineModuleAttribute<T> : WolverineModuleAttribute
     ///     Specify the IWolverineExtension type that should be automatically loaded
     ///     and applied when this assembly is present
     /// </summary>
-    /// <param name="wolverineExtensionType"></param>
-    /// <exception cref="ArgumentOutOfRangeException"></exception>
     public WolverineModuleAttribute() :
         base(typeof(T))
     {
