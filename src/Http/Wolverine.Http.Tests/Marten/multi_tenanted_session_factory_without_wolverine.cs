faf8c1310901a33071d6f3ef9b6b01bfeba848e9
Jeremy D. Miller <jeremydmiller@yahoo.com>
2024-06-04 10:54:17 -0500

    Little helper for metadata with HttpContext & Marten multi-tenancy

--- src/Http/Wolverine.Http.Tests/Marten/multi_tenanted_session_factory_without_wolverine.cs
+++ src/Http/Wolverine.Http.Tests/Marten/multi_tenanted_session_factory_without_wolverine.cs
@@ -74,6 +74,72 @@ public class multi_tenanted_session_factory_without_wolverine
         var greenDoc = await host.GetAsJson<ColorDoc>("/color?tenant=green");
         greenDoc.Number.ShouldBe(2);
     }
+    
+    [Fact]
+    public async Task can_do_the_tenancy_detection_with_custom_metadata()
+    {
+        // Arrange -- and sorry, it's a bit of "Arrange" to get an IHost
+        var builder = WebApplication.CreateBuilder([]);
+
+        builder.Services
+            .AddMarten(options =>
+            {
+                options.Connection(Servers.PostgresConnectionString);
+                options.DatabaseSchemaName = "mt_no_wolverine";
+                options.Schema.For<ColorDoc>().MultiTenanted();
+            });
+
+        #region sample_using_AddMartenTenancyDetection
+
+        builder.Services.AddMartenTenancyDetection(tenantId =>
+        {
+            tenantId.IsQueryStringValue("tenant");
+            tenantId.DefaultIs("default-tenant");
+        }, (c, session) =>
+        {
+            session.CorrelationId = c.TraceIdentifier;
+        });
+
+        #endregion
+        
+        // This is using Alba, which uses WebApplicationFactory under the covers
+        await using var host = await AlbaHost.For(builder, app =>
+        {
+            app.MapGet("/color", (IDocumentSession session) => session.LoadAsync<ColorDoc>("main"));
+        });
+
+        var store = host.DocumentStore();
+
+        // Store the blue doc
+        using (var session = store.LightweightSession("blue"))
+        {
+            session.Store(new ColorDoc
+            {
+                Id = "main",
+                Number = 1
+            });
+
+            await session.SaveChangesAsync();
+        }
+        
+        // Store the green doc
+        using (var session = store.LightweightSession("green"))
+        {
+            session.Store(new ColorDoc
+            {
+                Id = "main",
+                Number = 2
+            });
+
+            await session.SaveChangesAsync();
+        }
+
+        var blueDoc = await host.GetAsJson<ColorDoc>("/color?tenant=blue");
+        blueDoc.Number.ShouldBe(1);
+        
+        var greenDoc = await host.GetAsJson<ColorDoc>("/color?tenant=green");
+        greenDoc.Number.ShouldBe(2);
+    }
 }
 
 public class ColorDoc
