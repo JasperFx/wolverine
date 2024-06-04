9ec7553f73c850468d30fa30503403b5b780e56f
Daniel Häfele <haefele@xemio.net>
2024-06-04 21:48:08 +0200

    Add recommondation for [Document(Required = true)] over [Document][Required]

---
+++ src/Http/Wolverine.Http.Tests/Marten/document_attribute_required_usage.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shouldly;
+
+namespace Wolverine.Http.Tests.Marten;
+
+public class document_attribute_required_usage(AppFixture fixture) : IntegrationContext(fixture)
+{
+    [Fact]
+    public async Task separate_attributes_call_load_method()
+    {
+        var result = await Scenario(x =>
+        {
+            x.Get.Url("/document-required/separate-attributes/" + Guid.NewGuid());
+            x.StatusCodeShouldBe(404);
+        });
+
+        var problemDetails = await result.ReadAsJsonAsync<ProblemDetails>();
+        problemDetails.ShouldNotBeNull();
+        problemDetails.Title.ShouldBe("Invoice is not found");
+        problemDetails.Detail.ShouldBe("We only get here with [Document][Required]");
+    }
+    
+    [Fact]
+    public async Task document_attribute_only_does_not_call_load_method()
+    {
+        // This call gets short-circuited by DocumentAttribute.Required and thus the DocumentRequiredEndpoint.Load method is not called
+        var result = await Scenario(x =>
+        {
+            x.Get.Url("/document-required/document-attribute-only/" + Guid.NewGuid());
+            x.StatusCodeShouldBe(404);
+        });
+
+        var problemDetails = await result.ReadAsTextAsync();
+        problemDetails.ShouldBeEmpty();
+    }
+}
---
+++ src/Http/WolverineWebApi/Marten/DocumentRequiredEndpoint.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Wolverine.Http;
+using Wolverine.Http.Marten;
+
+namespace WolverineWebApi.Marten;
+
+public class DocumentRequiredEndpoint
+{
+    public static ProblemDetails Load(Invoice? invoice)
+    {
+        if (invoice is null)
+        {
+            return new ProblemDetails
+            {
+                Title = "Invoice is not found",
+                Detail = "We only get here with [Document][Required]",
+                Status = 404,
+            };
+        }
+
+        return WolverineContinue.NoProblems;
+    }
+    
+    [WolverineGet("document-required/separate-attributes/{id}")]
+    public static Invoice SeparateAttributes([Document][Required] Invoice invoice)
+    {
+        return invoice;
+    }
+    
+    [WolverineGet("document-required/document-attribute-only/{id}")]
+    public static Invoice DocumentAttributeOnly([Document(Required = true)] Invoice invoice)
+    {
+        return invoice;
+    }
+}
