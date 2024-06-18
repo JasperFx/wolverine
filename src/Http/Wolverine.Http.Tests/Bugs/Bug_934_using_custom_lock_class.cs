0c3479d58bfefc74a4fdc7e3c7bdec4a91dc5ec6
Jeremy D. Miller <jeremydmiller@yahoo.com>
2024-06-18 12:15:01 -0500

    Ability to do codegen against custom classes named "Lock". Closes 934

---
+++ src/Http/Wolverine.Http.Tests/Bugs/Bug_934_using_custom_lock_class.cs
@@ -0,0 +1,71 @@
+using Alba;
+using IntegrationTests;
+using Marten;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Wolverine.Marten;
+
+namespace Wolverine.Http.Tests.Bugs;
+
+public class Bug_934_using_custom_lock_class
+{
+    [Fact]
+    public async Task can_use_lock()
+    {
+        var builder = WebApplication.CreateBuilder([]);
+        
+        builder.Services.AddMarten(opts =>
+        {
+            // Establish the connection string to your Marten database
+            opts.Connection(Servers.PostgresConnectionString);
+            opts.DisableNpgsqlLogging = true;
+        }).IntegrateWithWolverine();
+        
+        builder.Host.UseWolverine(opts =>
+        {
+            opts.Discovery.DisableConventionalDiscovery();
+            opts.ApplicationAssembly = GetType().Assembly;
+        });
+        
+        // This is using Alba, which uses WebApplicationFactory under the covers
+        await using var host = await AlbaHost.For(builder, app =>
+        {
+            app.MapWolverineEndpoints();
+        });
+
+        await host.Scenario(x =>
+        {
+            x.Put.Url("/lock/one");
+            x.StatusCodeShouldBe(202);
+        });
+    }
+}
+
+public record Lock {
+    public required string Id;
+}
+
+public record ReservedKeywordBugResponse(string Id)
+    : AcceptResponse($"/lock/{Id}");
+
+public static class ReservedKeywordBugEndpoint
+{
+    public static Lock Load(
+        string id
+    )
+    {
+        
+        return new Lock { Id = id };
+    }
+
+    [WolverinePut("/lock/{lockId}")]
+    [ProducesResponseType(typeof(string), 400)]
+    public static ReservedKeywordBugResponse Handle(
+        string lockId
+    )
+    {
+        var response = new ReservedKeywordBugResponse(lockId);
+
+        return response;
+    }
+}
