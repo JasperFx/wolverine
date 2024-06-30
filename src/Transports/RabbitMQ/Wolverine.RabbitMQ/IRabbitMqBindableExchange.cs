029893a4ab41e02b1f479395b9304cb0cd052621
JT <Hawxy@users.noreply.github.com>
2024-06-30 23:06:25 +0800

    Cleanup

---
+++ src/Transports/RabbitMQ/Wolverine.RabbitMQ/IRabbitMqBindableExchange.cs
@@ -0,0 +1,21 @@
+﻿namespace Wolverine.RabbitMQ;
+
+// Legacy interface for exchange -> queue binding expressions
+public interface IRabbitMqBindableExchange : IRabbitMqExchange
+{
+    /// <summary>
+    /// Binds this exchange to a provided topic
+    /// </summary>
+    /// <param name="topicPattern"></param>
+    /// <returns></returns>
+    public TopicBindingExchange BindTopic(string topicPattern);
+
+    /// <summary>
+    /// Binds this exchange to the provided queue
+    /// </summary>
+    /// <param name="queueName"></param>
+    /// <param name="bindingKey"></param>
+    /// <returns></returns>
+    public RabbitMqBinding BindQueue(string queueName, string? bindingKey = null);
+    
+}
---
+++ src/Transports/RabbitMQ/Wolverine.RabbitMQ/RabbitMqExchangeConfigurationExpression.cs
@@ -0,0 +1,44 @@
+﻿namespace Wolverine.RabbitMQ.Internal;
+
+public sealed class RabbitMqExchangeConfigurationExpression : IRabbitMqBindableExchange
+{
+    private readonly RabbitMqExchange _exchange;
+    private readonly RabbitMqTransport _transport;
+
+    public RabbitMqExchangeConfigurationExpression(string exchangeName, RabbitMqTransport transport)
+    {
+        _exchange = transport.Exchanges[exchangeName];
+        _transport = transport;
+    }
+
+    public string Name => _exchange.Name;
+
+    public bool IsDurable
+    {
+        get => _exchange.IsDurable;
+        set => _exchange.IsDurable = value;
+    }
+    public ExchangeType ExchangeType
+    {
+        get => _exchange.ExchangeType;
+        set => _exchange.ExchangeType = value;
+    }
+    public bool AutoDelete
+    {
+        get => _exchange.AutoDelete;
+        set => _exchange.AutoDelete = value;
+    }
+    public IDictionary<string, object> Arguments => _exchange.Arguments;
+    public TopicBindingExchange BindTopic(string topicPattern)
+    {
+        var queue = _transport.Queues[Name];
+        queue.BindTopic(topicPattern);
+        return new TopicBindingExchange(_transport, topicPattern, Name);
+    }
+
+    public RabbitMqBinding BindQueue(string queueName, string? bindingKey = null)
+    {
+        var queue = _transport.Queues[queueName];
+        return queue.BindExchange(Name, bindingKey);
+    }
+}
