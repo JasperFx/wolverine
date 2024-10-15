8e8a096137a850784371ef95dddc9f923d8eb007
Jeremy D. Miller <jeremydmiller@yahoo.com>
2024-10-15 14:31:33 -0500

    Can use ProblemDetails with message handlers as well. Can use message handlers as HTTP endpoints. Closes GH-1069

--- src/Http/WolverineWebApi/ProblemDetailsUsage.cs
+++ src/Http/WolverineWebApi/ProblemDetailsUsage.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Wolverine.Http;
 
@@ -32,3 +33,35 @@ public class ProblemDetailsUsageEndpoint
 public record NumberMessage(int Number);
 
 #endregion
+
+public static class NumberMessageHandler
+{
+    #region sample_using_problem_details_in_message_handler
+
+    public static ProblemDetails Validate(NumberMessage message)
+    {
+        if (message.Number > 5)
+        {
+            return new ProblemDetails
+            {
+                Detail = "Number is bigger than 5",
+                Status = 400
+            };
+        }
+        
+        // All good, keep on going!
+        return WolverineContinue.NoProblems;
+    }
+    
+    // Look at this! You can use this as an HTTP endpoint too!
+    [WolverinePost("/problems2")]
+    public static void Handle(NumberMessage message)
+    {
+        Debug.WriteLine("Handled " + message);
+        Handled = true;
+    }
+
+    #endregion
+
+    public static bool Handled { get; set; }
+}
