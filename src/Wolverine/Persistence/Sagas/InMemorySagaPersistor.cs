84d279a3314e13dfdba20dacc3cbae140d45675b
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2024-10-24 15:58:40 +0200

    fix: throw a more meaningful error message if saga Id is not set

--- src/Wolverine/Persistence/Sagas/InMemorySagaPersistor.cs
+++ src/Wolverine/Persistence/Sagas/InMemorySagaPersistor.cs
@@ -35,7 +35,7 @@ public class InMemorySagaPersistor
         if (id == null)
         {
             throw new InvalidOperationException(
-                $"Type {typeof(T).FullNameInCode()} does not have a public Id property");
+                $"Type {typeof(T).FullNameInCode()} Id must be set to a non-null value.");
         }
 
         var key = ToKey(typeof(T), id);
