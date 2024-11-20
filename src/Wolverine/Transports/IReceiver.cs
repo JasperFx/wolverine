e9ce46806093d64334a92cc55c5b3e3ca0ce9fd3
Jeremy D. Miller <jeremydmiller@yahoo.com>
2024-11-20 09:28:00 -0600

    Added ReceiverWithRules for later

--- src/Wolverine/Transports/IReceiver.cs
+++ src/Wolverine/Transports/IReceiver.cs
@@ -7,3 +7,48 @@ public interface IReceiver : IDisposable
 
     ValueTask DrainAsync();
 }
+
+internal class ReceiverWithRules : IReceiver
+{
+    private readonly IReceiver _inner;
+    private readonly IEnvelopeRule[] _rules;
+
+    public ReceiverWithRules(IReceiver inner, IEnumerable<IEnvelopeRule> rules)
+    {
+        _inner = inner;
+        _rules = rules.ToArray();
+    }
+
+    public void Dispose()
+    {
+        _inner.Dispose();
+    }
+
+    public ValueTask ReceivedAsync(IListener listener, Envelope[] messages)
+    {
+        foreach (var envelope in messages)
+        {
+            foreach (var rule in _rules)
+            {
+                rule.Modify(envelope);
+            }
+        }
+
+        return _inner.ReceivedAsync(listener, messages);
+    }
+
+    public ValueTask ReceivedAsync(IListener listener, Envelope envelope)
+    {
+        foreach (var rule in _rules)
+        {
+            rule.Modify(envelope);
+        }
+
+        return _inner.ReceivedAsync(listener, envelope);
+    }
+
+    public async ValueTask DrainAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
