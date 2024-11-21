dc2d7b642431238bc6202a1f0d5c14f4b55f3741
Jeremy D. Miller <jeremydmiller@yahoo.com>
2024-11-21 08:23:42 -0600

    Ability to chain delivery option helper extension methods for cascading. Closes GH-1142

--- src/Testing/CoreTests/ConfiguredMessageExtensionsTests.cs
+++ src/Testing/CoreTests/ConfiguredMessageExtensionsTests.cs
@@ -20,6 +20,44 @@ public class ConfiguredMessageExtensionsTests
     }
 
     [Fact]
+    public void chain_delayed_for()
+    {
+        var delay = 5.Minutes();
+        var inner = new Message1();
+
+        var configured = inner.WithTenantId("one")
+            .DelayedFor(5.Minutes());
+        
+        configured.Options.ScheduleDelay.ShouldBe(delay);
+        configured.Message.ShouldBe(inner);
+        configured.Options.TenantId.ShouldBe("one");
+    }
+
+    [Fact]
+    public void chain_with_tenant_id()
+    {
+        var delay = 5.Minutes();
+        var inner = new Message1();
+
+        var configured = inner
+            .DelayedFor(delay).WithTenantId("one");
+        
+        configured.Options.ScheduleDelay.ShouldBe(delay);
+        configured.Options.TenantId.ShouldBe("one");
+    }
+
+    [Fact]
+    public void chain_with_group_id()
+    {
+        var inner = new Message1();
+        var configured = inner.WithTenantId("one")
+            .WithGroupId("g1");
+        
+        configured.Options.TenantId.ShouldBe("one");
+        configured.Options.GroupId.ShouldBe("g1");
+    }
+
+    [Fact]
     public void scheduled_at()
     {
         var time = (DateTimeOffset)DateTime.Today;
@@ -29,6 +67,19 @@ public class ConfiguredMessageExtensionsTests
 
         configured.Options.ScheduledTime.ShouldBe(time);
         configured.Message.ShouldBe(inner);
+    }
+    
+    [Fact]
+    public void chain_scheduled_at()
+    {
+        var time = (DateTimeOffset)DateTime.Today;
+        var inner = new Message1();
+
+        var configured = inner.WithTenantId("one").ScheduledAt(time);
+
+        configured.Options.ScheduledTime.ShouldBe(time);
+        configured.Message.ShouldBe(inner);
+        configured.Options.TenantId.ShouldBe("one");
     }
 
     [Fact]
@@ -66,5 +117,20 @@ public class ConfiguredMessageExtensionsTests
         await message.As<ISendMyself>().ApplyAsync(bus);
 
         await bus.Received().BroadcastToTopicAsync("blue", inner);
+    }
+    
+    [Fact]
+    public async Task chaining_to_topic()
+    {
+        var inner = new Message1();
+        var message = inner.WithTenantId("one").ToTopic("blue");
+        message.Message.ShouldBeSameAs(inner);
+        message.Topic.ShouldBe("blue");
+        message.Options.TenantId.ShouldBe("one");
+
+        var bus = Substitute.For<IMessageContext>();
+        await message.As<ISendMyself>().ApplyAsync(bus);
+
+        await bus.Received().BroadcastToTopicAsync("blue", inner, message.Options);
     }
 }
