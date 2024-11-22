066d7656491aa92dcd079aa5fa5fba9c3443f111
Jeremy D. Miller <jeremydmiller@yahoo.com>
2024-11-22 13:59:48 -0600

    Spiked in changes to opt Rabbit MQ into the multi-tenancy model

--- src/Wolverine/Transports/IListener.cs
+++ src/Wolverine/Transports/IListener.cs
@@ -11,3 +11,50 @@ public interface IListener : IChannelCallback, IAsyncDisposable
     /// <returns></returns>
     ValueTask StopAsync();
 }
+
+public class CompoundListener : IListener
+{
+    public readonly List<IListener> Inner = new();
+
+    public CompoundListener(Uri address)
+    {
+        Address = address;
+    }
+
+    public ValueTask CompleteAsync(Envelope envelope)
+    {
+        throw new NotSupportedException();
+    }
+
+    public ValueTask DeferAsync(Envelope envelope)
+    {
+        throw new NotSupportedException();
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        foreach (var listener in Inner)
+        {
+            await listener.DisposeAsync();
+        }
+    }
+
+    public Uri Address { get; }
+    public async ValueTask StopAsync()
+    {
+        var exceptions = new List<Exception>();
+        foreach (var listener in Inner)
+        {
+            try
+            {
+                await listener.StopAsync();
+            }
+            catch (Exception e)
+            {
+                exceptions.Add(e);
+            }
+        }
+
+        if (exceptions.Any()) throw new AggregateException(exceptions);
+    }
+}
