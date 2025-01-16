9e90c6cf262fffc1352798522752793c2f4f19f3
Jeremy D. Miller <jeremydmiller@yahoo.com>
2025-01-16 06:05:54 -0600

    wip

---
+++ src/Wolverine/Configuration/IConfigureLocalQueue.cs
@@ -0,0 +1,13 @@
+using Wolverine.Transports.Local;
+
+namespace Wolverine.Configuration;
+
+/// <summary>
+/// Helps mark a handler to configure the local queue that its messages
+/// would be routed to. It's probably only useful to use this with "sticky" handlers
+/// that run on an isolated local queue
+/// </summary>
+public interface IConfigureLocalQueue
+{
+    static abstract void  Configure(LocalQueueConfiguration configuration);
+}
