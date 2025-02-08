0b59e53d64e5bda69205f2dbd6646da460b0b737
Jeremy D. Miller <jeremydmiller@yahoo.com>
2025-02-08 15:00:30 -0600

    Upgrade to Marten 8 alpha and JasperFx dependencies. Beginning of Wolverine 4

--- src/Testing/CoreTests/Internal/Generated/WolverineHandlers/CSP4Handler299410023.cs
+++ src/Testing/CoreTests/Internal/Generated/WolverineHandlers/CSP4Handler299410023.cs
@@ -5,7 +5,8 @@ using Microsoft.Extensions.DependencyInjection;
 namespace Internal.Generated.WolverineHandlers
 {
     // START: CSP4Handler299410023
-    public class CSP4Handler299410023 : Wolverine.Runtime.Handlers.MessageHandler
+    [global::System.CodeDom.Compiler.GeneratedCode("JasperFx", "1.0.0")]
+    public sealed class CSP4Handler299410023 : Wolverine.Runtime.Handlers.MessageHandler
     {
         private readonly Microsoft.Extensions.DependencyInjection.IServiceScopeFactory _serviceScopeFactory;
 
