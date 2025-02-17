0cccac8c4ae7397f64b09eb40375881ec112f80f
Jeremy D. Miller <jeremydmiller@yahoo.com>
2025-02-17 15:21:26 -0600

    Reorganizing the code that will live in a monitored app vs what lives in CritterWatch itself

--- src/Wolverine/AssemblyAttributes.cs
+++ src/Wolverine/AssemblyAttributes.cs
@@ -8,7 +8,7 @@ using Wolverine.Attributes;
 [assembly: WolverineFeature]
 
 [assembly: InternalsVisibleTo("CoreTests")]
-[assembly: InternalsVisibleTo("CritterWatch.Services")]
+[assembly: InternalsVisibleTo("Wolverine.CritterWatch")]
 [assembly: InternalsVisibleTo("DataGenerator")]
 [assembly: InternalsVisibleTo("DiagnosticsTests")]
 [assembly: InternalsVisibleTo("PolicyTests")]
