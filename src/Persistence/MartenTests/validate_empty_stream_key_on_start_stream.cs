070b9c7d8ad88bf51d9abce5cdc716434bd77a82
Jeremy D. Miller <jeremydmiller@yahoo.com>
2025-02-18 08:11:21 -0600

    MartenOps.StartStream() can guard against empty or null stream keys when using string identified streams. Closes GH-1269

---
+++ src/Persistence/MartenTests/validate_empty_stream_key_on_start_stream.cs
@@ -0,0 +1,69 @@
+using IntegrationTests;
+using Marten;
+using Marten.Events;
+using MartenTests.Distribution.TripDomain;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Oakton.Resources;
+using Shouldly;
+using Wolverine;
+using Wolverine.Marten;
+
+namespace MartenTests;
+
+public class validate_empty_stream_key_on_start_stream: PostgresqlContext, IAsyncLifetime
+{
+    private IHost _host;
+    private IDocumentStore _store;
+
+    public async Task InitializeAsync()
+    {
+        _host = await Host.CreateDefaultBuilder()
+            .UseWolverine(opts =>
+            {
+                opts.Services
+                    .AddMarten(m =>
+                    {
+                        m.Connection(Servers.PostgresConnectionString);
+                        m.DatabaseSchemaName = "start_stream";
+                        m.Events.StreamIdentity = StreamIdentity.AsString;
+                    })
+                    .IntegrateWithWolverine();
+
+                opts.Policies.AutoApplyTransactions();
+
+                opts.Services.AddResourceSetupOnStartup();
+            }).StartAsync();
+
+        _store = _host.Services.GetRequiredService<IDocumentStore>();
+
+        await _store.Advanced.Clean.DeleteDocumentsByTypeAsync(typeof(NamedDocument));
+    }
+
+    public async Task DisposeAsync()
+    {
+        await _host.StopAsync();
+        _host.Dispose();
+    }
+
+    [Fact]
+    public void assert_empty_stream_key()
+    {
+        using var session = _store.LightweightSession();
+        
+        // No stream key supplied
+        var op = MartenOps.StartStream<Trip>(new TripStarted());
+
+        Should.Throw<InvalidOperationException>(() => op.Execute(session));
+    }
+
+    [Fact]
+    public void happy_path_execution()
+    {
+        using var session = _store.LightweightSession();
+        
+        // No stream key supplied
+        var op = MartenOps.StartStream<Trip>("a good key", new TripStarted());
+        op.Execute(session);
+    }
+}
