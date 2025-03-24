cf279c0949db972d4d0b8bd94fd46c332b83031d
Jeremy D. Miller <jeremydmiller@yahoo.com>
2025-03-24 14:09:47 -0500

    Correctly honoring [AlwaysPublishResponse] attribute. Closes GH-1306

--- src/Persistence/MartenTests/Bugs/Bug_305_invoke_async_with_return_not_publishing_with_tuple_return_value.cs
+++ src/Persistence/MartenTests/Bugs/Bug_305_invoke_async_with_return_not_publishing_with_tuple_return_value.cs
@@ -29,6 +29,32 @@ public class Bug_305_invoke_async_with_return_not_publishing_with_tuple_return_v
         tracked.Sent.SingleMessage<ItemCreated>().Name.ShouldBe("Trevor");
         tracked.Sent.SingleMessage<SecondItemCreated>().Name.ShouldBe("Trevor");
     }
+
+    [Fact]
+    public async Task honor_the_attribute()
+    {
+        using var host = await Host.CreateDefaultBuilder()
+            .UseWolverine(opts =>
+            {
+                opts.Services.AddMarten(Servers.PostgresConnectionString).IntegrateWithWolverine();
+                // Add the auto transaction middleware attachment policy
+                opts.Policies.AutoApplyTransactions();
+            }).StartAsync();
+
+        Func<IMessageContext, Task> execute = async c =>
+        {
+            var m2 = await c.InvokeAsync<AlwaysMessage2>(new AlwaysMessage1());
+            m2.ShouldNotBeNull();
+        };
+
+        var tracked = await host
+            .TrackActivity()
+            .WaitForMessageToBeReceivedAt<AlwaysMessage3>(host)
+            .ExecuteAndWaitAsync(execute);
+
+        tracked.Executed.SingleMessage<AlwaysMessage2>().ShouldNotBeNull();
+        tracked.Executed.SingleMessage<AlwaysMessage3>().ShouldNotBeNull();
+    }
 }
 
 public class CreateItemCommand
@@ -58,6 +84,39 @@ public class CreateItemCommandHandler
 }
 
 #endregion
+
+public record AlwaysMessage1;
+public record AlwaysMessage2;
+public record AlwaysMessage3;
+
+public static class AlwaysMessageHandler
+{
+    [AlwaysPublishResponse]
+    public static AlwaysMessage2 Handle(AlwaysMessage1 request, IMessageBus messageBus)
+    {
+        Console.WriteLine("Message1Handler");
+
+        return new AlwaysMessage2();
+    }
+}
+
+public static class Message2Handler
+{
+    public static AlwaysMessage3 Handle(AlwaysMessage2 request)
+    {
+        Console.WriteLine("Message2Handler");
+
+        return new AlwaysMessage3();
+    }
+}
+
+public static class Message3Handler
+{
+    public static void Handle(AlwaysMessage3 request)
+    {
+        Console.WriteLine("Message3Handler");
+    }
+}
 
 public record ItemCreated(Guid Id, string Name);
 
