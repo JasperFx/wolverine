6bee39161bfe60891e94f4119d8900832b6d682e
Shield1739 <18101480+Shield1739@users.noreply.github.com>
2025-03-29 18:01:06 -0500

    Adds new Amazon SNS Transport

---
+++ src/Transports/AWS/Wolverine.AmazonSns/AmazonSnsSubscriberConfiguration.cs
@@ -0,0 +1,51 @@
+﻿using System.Text.Json;
+using Amazon.SimpleNotificationService.Model;
+using Wolverine.AmazonSns.Internal;
+using Wolverine.Configuration;
+
+namespace Wolverine.AmazonSns;
+
+public class
+    AmazonSnsSubscriberConfiguration : SubscriberConfiguration<AmazonSnsSubscriberConfiguration, AmazonSnsTopic>
+{
+    internal AmazonSnsSubscriberConfiguration(AmazonSnsTopic endpoint) : base(endpoint)
+    {
+    }
+
+    /// <summary>
+    ///     Configure how the topic should be created within SNS
+    /// </summary>
+    /// <param name="configure"></param>
+    /// <returns></returns>
+    public AmazonSnsSubscriberConfiguration ConfigureTopicCreation(Action<CreateTopicRequest> configure)
+    {
+        add(e => configure(e.Configuration));
+        return this;
+    }
+
+    /// Opt to send messages as raw JSON without any Wolverine metadata
+    /// </summary>
+    /// <param name="defaultMessageType">Optional. If both sending and receiving from this queue, you will want to specify a default message type</param>
+    /// <param name="configure">Optional configuration of System.Text.Json for this endpoint</param>
+    /// <returns></returns>
+    public AmazonSnsSubscriberConfiguration SendRawJsonMessage(Type? defaultMessageType = null,
+        Action<JsonSerializerOptions>? configure = null)
+    {
+        var options = new JsonSerializerOptions();
+        configure?.Invoke(options);
+        add(e => e.Mapper = new RawJsonSnsEnvelopeMapper(defaultMessageType ?? typeof(object), options));
+
+        return this;
+    }
+
+    /// <summary>
+    /// Utilize custom envelope mapping for SNS interoperability with external non-Wolverine systems
+    /// </summary>
+    /// <param name="mapper"></param>
+    /// <returns></returns>
+    public AmazonSnsSubscriberConfiguration InteropWith(ISnsEnvelopeMapper mapper)
+    {
+        add(e => e.Mapper = mapper);
+        return this;
+    }
+}
---
+++ src/Transports/AWS/Wolverine.AmazonSns/Internal/AmazonSnsTransport.cs
@@ -0,0 +1,105 @@
+﻿using Amazon.Runtime;
+using Amazon.SimpleNotificationService;
+using JasperFx.Core;
+using Wolverine.Runtime;
+using Wolverine.Transports;
+
+namespace Wolverine.AmazonSns.Internal;
+
+public class AmazonSnsTransport : BrokerTransport<AmazonSnsTopic>
+{
+    public const string SnsProtocol = "sns";
+    
+    public const char Separator = '-';
+    
+    public AmazonSnsTransport() : base(SnsProtocol, "Amazon SNS")
+    {
+        Topics = new LightweightCache<string, AmazonSnsTopic>(name => new AmazonSnsTopic(name, this));
+        
+        IdentifierDelimiter = "-";
+    }
+    
+    internal AmazonSnsTransport(IAmazonSimpleNotificationService sqsClient) : this()
+    {
+        Client = sqsClient;
+    }
+    
+    public Func<IWolverineRuntime, AWSCredentials>? CredentialSource { get; set; }
+    public LightweightCache<string, AmazonSnsTopic> Topics { get; }
+    
+    public AmazonSimpleNotificationServiceConfig Config { get; } = new();
+    
+    internal IAmazonSimpleNotificationService? Client { get; private set; }
+
+    public int LocalStackPort { get; set; }
+
+    public bool UseLocalStackInDevelopment { get; set; }
+
+    // TODO duplicated code in SqsTransport
+    public static string SanitizeSnsName(string identifier)
+    {
+        //AWS requires FIFO topics to have a `.fifo` suffix
+        var suffixIndex = identifier.LastIndexOf(".fifo", StringComparison.OrdinalIgnoreCase);
+
+        if (suffixIndex != -1) // ".fifo" suffix found
+        {
+            var prefix = identifier[..suffixIndex];
+            var suffix = identifier[suffixIndex..];
+
+            prefix = prefix.Replace('.', Separator);
+
+            return prefix + suffix;
+        }
+
+        // ".fifo" suffix not found
+        return identifier.Replace('.', Separator);
+    }
+
+    public override string SanitizeIdentifier(string identifier)
+    {
+        return SanitizeSnsName(identifier);
+    }
+    
+    protected override IEnumerable<AmazonSnsTopic> endpoints()
+    {
+        return Topics;
+    }
+
+    protected override AmazonSnsTopic findEndpointByUri(Uri uri)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override ValueTask ConnectAsync(IWolverineRuntime runtime)
+    {
+        Client ??= BuildClient(runtime);
+        return ValueTask.CompletedTask;
+    }
+
+    public override IEnumerable<PropertyColumn> DiagnosticColumns()
+    {
+        throw new NotImplementedException();
+    }
+    
+    internal AmazonSnsTopic EndpointForTopic(string topicName)
+    {
+        return Topics[topicName];
+    }
+    
+    internal void ConnectToLocalStack(int port = 4566)
+    {
+        CredentialSource = _ => new BasicAWSCredentials("ignore", "ignore");
+        Config.ServiceURL = $"http://localhost:{port}";
+    }
+    
+    private IAmazonSimpleNotificationService BuildClient(IWolverineRuntime runtime)
+    {
+        if (CredentialSource == null)
+        {
+            return new AmazonSimpleNotificationServiceClient(Config);
+        }
+
+        var credentials = CredentialSource(runtime);
+        return new AmazonSimpleNotificationServiceClient(credentials, Config);
+    }
+}
