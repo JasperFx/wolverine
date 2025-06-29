46d383ac354e26cc5454c3ef1ef619f0113ee12f
Jeremy D. Miller <jeremydmiller@yahoo.com>
2025-06-29 12:41:59 -0500

    Getting the ServiceControlUri through ServiceCapabilities

--- src/Wolverine/Configuration/IListenerConfiguration.cs
+++ src/Wolverine/Configuration/IListenerConfiguration.cs
@@ -116,6 +116,12 @@ public interface IListenerConfiguration<T> : IEndpointConfiguration<T>
     /// <param name="handlerType"></param>
     /// <returns></returns>
     T AddStickyHandler(Type handlerType);
+
+    /// <summary>
+    /// "Pin" this endpoint so that it is only active on the leader node
+    /// </summary>
+    /// <returns></returns>
+    public T ListenOnlyAtLeader();
 }
 
 public interface IListenerConfiguration : IListenerConfiguration<IListenerConfiguration>;
