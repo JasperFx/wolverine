db7d6e004430b3042fcb2a532534e8b3527ebb43
Jeremy D. Miller <jeremydmiller@yahoo.com>
2025-07-10 09:15:44 -0500

    Making sure that messages get Ack'd in the combination of Inline endpoints with native DLQ disabled

--- src/Wolverine/Runtime/MessageContext.cs
+++ src/Wolverine/Runtime/MessageContext.cs
@@ -152,7 +152,7 @@ public class MessageContext : MessageBus, IMessageContext, IEnvelopeTransaction,
             throw new InvalidOperationException("No Envelope is active for this context");
         }
 
-        if (_channel is ISupportDeadLetterQueue c && c.NativeDeadLetterQueueEnabled)
+        if (_channel is ISupportDeadLetterQueue { NativeDeadLetterQueueEnabled: true } c)
         {
             if (Envelope.Batch != null)
             {
@@ -181,6 +181,9 @@ public class MessageContext : MessageBus, IMessageContext, IEnvelopeTransaction,
             // If persistable, persist
             await Storage.Inbox.MoveToDeadLetterStorageAsync(Envelope, exception);
         }
+
+        // If this is Inline
+        await _channel.CompleteAsync(Envelope);
     }
 
     public Task RetryExecutionNowAsync()
