a492c1000619e7f169955db139b54668341a095a
Jeremy D. Miller <jeremydmiller@yahoo.com>
2025-09-04 19:18:06 -0500

    Integration test for RespondToCaller()

---
+++ src/Transports/SignalR/Wolverine.SignalR.Tests/respond_to_originator.cs
@@ -0,0 +1,35 @@
+using System.Diagnostics;
+using Shouldly;
+using Wolverine.Tracking;
+
+namespace Wolverine.SignalR.Tests;
+
+public class respond_to_originator : WebSocketTestContext
+{
+    [Fact]
+    public async Task send_to_the_originating_connection()
+    {
+        var green = await StartClientHost("green");
+        var red = await StartClientHost("red");
+        var blue = await StartClientHost("blue");
+
+        var tracked = await red.TrackActivity()
+            .IncludeExternalTransports()
+            .AlsoTrack(theWebApp)
+            .SendMessageAndWaitAsync(new RequiresResponse("Leo Chenal"));
+
+        var record = tracked.Executed.SingleRecord<WebSocketResponse>();
+        record.ServiceName.ShouldBe("red");
+        record.Message.ShouldBeOfType<WebSocketResponse>().Name.ShouldBe("Leo Chenal");
+    }
+}
+
+public record RequiresResponse(string Name) : WebSocketMessage;
+public record WebSocketResponse(string Name) : WebSocketMessage;
+
+public static class ResponseHandler
+{
+    public static object Handle(RequiresResponse msg) => new WebSocketResponse(msg.Name).RespondToCaller();
+
+    public static void Handle(WebSocketResponse msg) => Debug.WriteLine(msg);
+}
