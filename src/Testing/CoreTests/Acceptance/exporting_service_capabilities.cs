96f9265f250245e54c6cacd114b9e9987ab66f48
Jeremy D. Miller <jeremydmiller@yahoo.com>
2025-12-12 11:36:53 -0600

    Simple tests on ServiceCapabilities picking up the application and Wolverine version

--- src/Testing/CoreTests/Acceptance/exporting_service_capabilities.cs
+++ src/Testing/CoreTests/Acceptance/exporting_service_capabilities.cs
@@ -1,19 +1,41 @@
-using JasperFx;
-using Microsoft.Extensions.Hosting;
+using Wolverine.Configuration.Capabilities;
+using Wolverine.Tracking;
 using Xunit;
 
 namespace CoreTests.Acceptance;
 
-public class exporting_service_capabilities
+public class exporting_service_capabilities : IntegrationContext, IAsyncLifetime
 {
-    [Fact]
-    public async Task smoke_test_the_command()
-    {
-        var builder = Host.CreateDefaultBuilder()
-            .UseWolverine();
+    private ServiceCapabilities theCapabilities;
 
-        var exitCode = await builder.RunJasperFxCommands(["capabilities"]);
+    public exporting_service_capabilities(DefaultApp @default) : base(@default)
+    {
         
-        exitCode.ShouldBe(0);
     }
+    
+
+    public async Task InitializeAsync()
+    {
+        theCapabilities = await ServiceCapabilities.ReadFrom(Host.GetRuntime(), null, CancellationToken.None);
+    }
+
+    public Task DisposeAsync()
+    {
+        return Task.CompletedTask;
+    }
+
+    [Fact]
+    public void wolverine_version()
+    {
+        theCapabilities.WolverineVersion.ShouldBe(typeof(WolverineOptions).Assembly.GetName().Version);
+    }
+
+    [Fact]
+    public void the_application_version()
+    {
+        theCapabilities.Version.ShouldBe(GetType().Assembly.GetName().Version);
+    }
+    
+    
+
 }
