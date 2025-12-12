ddb69a9de8a186b1edd3c954eb0cfbee46283a96
Jeremy D. Miller <jeremydmiller@yahoo.com>
2025-12-12 12:01:22 -0600

    Made the SystemCapabilities ordering of its children be consistent. Closes GH-1943

--- src/Wolverine/Configuration/Capabilities/MessageStore.cs
+++ src/Wolverine/Configuration/Capabilities/MessageStore.cs
@@ -1,7 +1,17 @@
 
 
 using JasperFx.Descriptors;
+using Wolverine.Persistence.Durability;
 
 namespace Wolverine.Configuration.Capabilities;
 
-public record MessageStore(Uri Uri, bool SupportsDeadLetterQueueAdmin, DatabaseDescriptor Database);
+public record MessageStore(
+    Uri Uri,
+    MessageStoreRole Role,
+    DatabaseDescriptor Database)
+{
+    public static MessageStore For(IMessageStore store)
+    {
+        return new (store.Uri, store.Role, store.Describe());
+    }
+}
