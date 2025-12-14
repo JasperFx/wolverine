c9d08008ebaac47c6219f9bac4a028ea0bf70dc7
Anton Liljeberg <ab.liljeberg.anton@gmail.com>
2025-12-14 12:33:28 +0100

    feat: saga identity from attribute

--- src/Testing/CoreTests/Persistence/Sagas/saga_id_member_determination.cs
+++ src/Testing/CoreTests/Persistence/Sagas/saga_id_member_determination.cs
@@ -10,17 +10,19 @@ public class saga_id_member_determination
     [InlineData(typeof(SomeSagaMessage2), nameof(SomeSagaMessage2.SagaId))]
     [InlineData(typeof(SomeSagaMessage3), nameof(SomeSagaMessage3.SomeSagaId))]
     [InlineData(typeof(SomeSagaMessage4), nameof(SomeSagaMessage4.Id))]
+    [InlineData(typeof(SomeSagaMessage5), nameof(SomeSagaMessage5.SomeId))]
     public void determine_the_member(Type messageType, string expectedMemberName)
     {
-        SagaChain.DetermineSagaIdMember(messageType, typeof(SomeSaga)).Name
+        SagaChain.DetermineSagaIdMember(messageType, typeof(SomeSaga))?.Name
             .ShouldBe(expectedMemberName);
     }
 }
 
-public record SomeSagaMessage1(Guid Id, [property: SagaIdentity]Guid RandomName);
+public record SomeSagaMessage1(Guid Id, [property: SagaIdentity] Guid RandomName);
 public record SomeSagaMessage2(Guid SagaId, Guid Id);
 public record SomeSagaMessage3(Guid Id, Guid SomeSagaId, Guid SagaId);
 public record SomeSagaMessage4(Guid Id);
+public record SomeSagaMessage5(Guid SomeId);
 
 
 public class SomeSaga
