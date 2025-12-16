7594d57fe5fab32a3be0244000a334c9743425cb
Jeremy D. Miller <jeremydmiller@yahoo.com>
2025-12-16 07:45:10 -0600

    Fixed the log format issue. Closes GH-1963

--- src/Wolverine/Logging/LogStartingActivity.cs
+++ src/Wolverine/Logging/LogStartingActivity.cs
@@ -42,7 +42,7 @@ internal class LogStartingActivity : SyncFrame
     public override void GenerateCode(GeneratedMethod method, ISourceWriter writer)
     {
         writer.WriteComment("Application specific auditing");
-        var template = _members.Any(x => x.Member.Name.EqualsIgnoreCase("id")) ? "$Starting to process {_inputType.FullNameInCode()} ({{EnvelopeId}}" : $"Starting to process {_inputType.FullNameInCode()} ({{Id}})" ;
+        var template = _members.Any(x => x.Member.Name.EqualsIgnoreCase("id")) ? $"Starting to process {_inputType.FullNameInCode()} ({{EnvelopeId}}" : $"Starting to process {_inputType.FullNameInCode()} ({{Id}})" ;
         if (_members.Count != 0)
         {
             template += " with " + _members.Select(m => $"{m.MemberName}: {{{m.Member.Name}}}").Join(", ");
