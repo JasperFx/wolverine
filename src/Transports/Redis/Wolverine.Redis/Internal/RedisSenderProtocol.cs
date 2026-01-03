1128c5510e4a4656ceee58c350def713f4c48af2
sdesaiLULA <sandeep@lula.is>
2026-01-03 14:19:08 -0500

    Implement Redis transport scheduling for messages; add support for immediate and scheduled sends, and integrate tests for scheduled message flow

--- src/Transports/Redis/Wolverine.Redis/Internal/RedisSenderProtocol.cs
+++ src/Transports/Redis/Wolverine.Redis/Internal/RedisSenderProtocol.cs
@@ -1,4 +1,5 @@
 using StackExchange.Redis;
+using Wolverine.Runtime.Serialization;
 using Wolverine.Transports;
 using Wolverine.Transports.Sending;
 
@@ -17,19 +18,33 @@ public class RedisSenderProtocol : ISenderProtocol, IDisposable
 
     public async Task SendBatchAsync(ISenderCallback callback, OutgoingMessageBatch batch)
     {
-        var database = _transport.GetDatabase();
+        var database = _transport.GetDatabase(database: _endpoint.DatabaseId);
 
         try
         {
-            var tasks = new List<Task<RedisValue>>(batch.Messages.Count);
+            var immediateTasks = new List<Task<RedisValue>>();
+            var scheduledTasks = new List<Task<bool>>();
+            
             foreach (var envelope in batch.Messages)
             {
-                var list = new List<NameValueEntry>();
-                _endpoint.EnvelopeMapper!.MapEnvelopeToOutgoing(envelope, list);
-                tasks.Add(database.StreamAddAsync(_endpoint.StreamKey, list.ToArray()));
+                if (envelope.IsScheduledForLater(DateTimeOffset.UtcNow))
+                {
+                    // Add to scheduled sorted set
+                    var scheduledKey = _endpoint.ScheduledMessagesKey;
+                    var serializedEnvelope = EnvelopeSerializer.Serialize(envelope);
+                    var score = envelope.ScheduledTime!.Value.ToUnixTimeMilliseconds();
+                    scheduledTasks.Add(database.SortedSetAddAsync(scheduledKey, serializedEnvelope, score));
+                }
+                else
+                {
+                    // Send immediately to stream
+                    var list = new List<NameValueEntry>();
+                    _endpoint.EnvelopeMapper!.MapEnvelopeToOutgoing(envelope, list);
+                    immediateTasks.Add(database.StreamAddAsync(_endpoint.StreamKey, list.ToArray()));
+                }
             }
 
-            await Task.WhenAll(tasks);
+            await Task.WhenAll(immediateTasks.Cast<Task>().Concat(scheduledTasks.Cast<Task>()));
             await callback.MarkSuccessfulAsync(batch);
         }
         catch (Exception ex)
