ea419e5b5d75468b0ab0fdbbae076424513519a6
Abir Mahmud <58890389+mahmudabir@users.noreply.github.com>
2026-01-27 21:22:55 +0600

    Added Test for PR#2076

---
+++ src/Testing/MessageRoutingTests/publish_message_with_type_parameter.cs
@@ -0,0 +1,18 @@
+﻿using Wolverine;
+using Xunit;
+
+namespace MessageRoutingTests;
+
+public class publish_message_with_type_parameter : MessageRoutingContext
+{
+    protected override void configure(WolverineOptions opts)
+    {
+        opts.PublishMessage(typeof(M1)).ToLocalQueue("blue");
+    }
+    
+    [Fact]
+    public void locally_handled_messages_get_overridden_by_routing()
+    {
+        assertRoutesAre<M1>("local://blue");
+    }
+}
