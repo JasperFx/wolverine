524751294dff0de93e5f0309e817feb2bcc9337c
Erik Shafer <erik.shafer@gmail.com>
2026-03-10 01:15:41 -0500

    assembly attributes fix

--- src/Http/Wolverine.Http.Grpc/AssemblyAttributes.cs
+++ src/Http/Wolverine.Http.Grpc/AssemblyAttributes.cs
@@ -1,5 +1,5 @@
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 using JasperFx.Core.TypeScanning;
 
-[assembly: IgnoreAssembly]
 [assembly: InternalsVisibleTo("Wolverine.Http.Grpc.Tests")]
+[assembly: IgnoreAssembly]
