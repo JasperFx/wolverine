b8c72b967f5e86fa98bdf1e63dda8ef01fcc9b12
ferdavs <ferdavs@naver.com>
2026-05-05 18:19:08 +0500

    Fixes the test

--- src/Persistence/EfCoreTests/eager_idempotency_with_non_wolverine_mapped_db_context.cs
+++ src/Persistence/EfCoreTests/eager_idempotency_with_non_wolverine_mapped_db_context.cs
@@ -106,6 +106,7 @@ public class eager_idempotency_with_non_wolverine_mapped_db_context : IClassFixt
         };
 
         var transaction = new EfCoreEnvelopeTransaction(dbContext, context);
+        await dbContext.Database.BeginTransactionAsync();
         await transaction.PersistOutgoingAsync([envelope1, envelope2]);
         await dbContext.Database.CurrentTransaction!.CommitAsync();
 
