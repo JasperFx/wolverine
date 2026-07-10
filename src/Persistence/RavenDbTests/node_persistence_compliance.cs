2fe097d1f28dc8bd68f85cd793254bfa1282c78a
Daniel Winkler <dani.winkler@gmail.com>
2026-07-10 17:06:37 +0200

    fix(RavenDB): Node sequence startup race (#3352)
    * fix(RavenDB): Node sequence startup race
    
    * fix(RavenDB): Improve node sequence concurrency handling with linear backoff and jitter

--- src/Persistence/RavenDbTests/node_persistence_compliance.cs
+++ src/Persistence/RavenDbTests/node_persistence_compliance.cs
@@ -1,7 +1,9 @@
 using Raven.Embedded;
+using Shouldly;
 using Wolverine;
 using Wolverine.ComplianceTests;
 using Wolverine.Persistence.Durability;
+using Wolverine.Runtime.Agents;
 using Wolverine.RavenDb.Internals;
 
 namespace RavenDbTests;
@@ -20,5 +22,39 @@ public class node_persistence_compliance : NodePersistenceCompliance
     {
         var store = _fixture.StartRavenStore();
         return new RavenDbMessageStore(store, new WolverineOptions());
+    }
+
+    [Fact]
+    public async Task concurrently_persisting_nodes_assigns_unique_node_numbers()
+    {
+        await using var messageStore = await buildCleanMessageStore();
+
+        var start = new TaskCompletionSource(TaskCreationOptions.RunContinuationsAsynchronously);
+        var nodes = Enumerable.Range(0, 20).Select(_ =>
+        {
+            var id = Guid.NewGuid();
+            return new WolverineNode
+            {
+                NodeId = id,
+                ControlUri = new Uri($"dbcontrol://{id}"),
+                Description = Environment.MachineName,
+                Version = new Version(1, 2, 3, 0)
+            };
+        }).ToArray();
+
+        var tasks = nodes.Select(async node =>
+        {
+            await start.Task;
+            return await messageStore.Nodes.PersistAsync(node, CancellationToken.None);
+        }).ToArray();
+
+        start.SetResult();
+
+        var assignedNodeNumbers = await Task.WhenAll(tasks);
+
+        assignedNodeNumbers.OrderBy(x => x).ShouldBe(Enumerable.Range(1, nodes.Length).ToArray());
+
+        var persisted = await messageStore.Nodes.LoadAllNodesAsync(CancellationToken.None);
+        persisted.Select(x => x.AssignedNodeNumber).OrderBy(x => x).ShouldBe(assignedNodeNumbers.OrderBy(x => x));
     }
 }
